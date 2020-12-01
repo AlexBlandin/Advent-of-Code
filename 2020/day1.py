d75258a64670b9ceaa9b5cc8f7930daa2de578bc
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-01 13:41:11 +0000

    Fear.

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,7 +1,17 @@
+*l,=map(int, open("day1.txt").readlines())
 i={}
-for x in map(int, open("day1.txt").readlines()):
+for x in l:
   if x not in i:
     i[2020-x]=x
   else:
     print(f"{x = }, {i[x] = }, {x + i[x] = }, {x * i[x] = }")
     break
+
+from itertools import combinations
+from functools import reduce
+from operator import mul
+print(list(filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(l,3))))[0][1])
+
+print()
+print("Now for the obligatory oneliner")
+[print(list(map(lambda x:x[1],filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(map(int, open("day1.txt").readlines()),n)))))[0]) for n in (2,3)]
