b03c8e8673d917be496794333b5866847b384612
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-03 16:09:44 +0000

    Cleanup

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,9 +1,6 @@
-m=[]
-for line in open("day3.txt").readlines(): m.append([0 if c=="." else 1 for c in line.strip()])
-T=1
+T, m = 1, [[0 if c=="." else 1 for c in line.strip()] for line in open("day3.txt").readlines()]
 for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]:
-  x,y=0,0
-  t=0
+  x,y,t=0,0,0
   while y < len(m):
     t+=m[y][x]
     x,y = (x+r)%31,y+d
