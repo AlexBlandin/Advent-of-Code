2db0db6e449c2ecaa6bdfc3f683c5fd210ca328c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-03 16:11:34 +0000

    Cleanup

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,9 +1,9 @@
 T, m = 1, [[0 if c=="." else 1 for c in line.strip()] for line in open("day3.txt").readlines()]
 for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]:
-  x,y,t=0,0,0
-  while y < len(m):
-    t+=m[y][x]
-    x,y = (x+r)%31,y+d
+  x,y,t = 0,0,0
+  for y in range(0,len(m),d):
+    t += m[y][x]
+    x = (x+r)%31
   T *= t
   if (r,d)==(3,1): print(t)
 print(T)
