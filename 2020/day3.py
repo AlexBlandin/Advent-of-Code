5d9919db64410c48c2cf72f80340286dc9736d57
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-03 22:07:57 +0000

    Thanks I hate it

--- 2020/day3.py
+++ 2020/day3.py
@@ -5,3 +5,6 @@ m = [[0 if c=="." else 1 for c in line.strip()] for line in open("day3.txt").rea
 t = [sum(m[y][x] for x,y in zip(map(lambda x:x%31, count(0,r)), range(0,323,d))) for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]]
 print(t[1])
 print(reduce(mul,t))
+
+# or, just part 2 with file io as 1liner, no preproc just horrific seeks
+with open("day3.txt") as m: print(reduce(mul, [sum(m.read(m.seek(y*32+x)*0+1)=="#" for x,y in zip(map(lambda x:x%31, count(0,r)), range(0,323,d))) for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]]))
