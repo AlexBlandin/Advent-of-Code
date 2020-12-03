9bfd27af17222c6a9f5acfe844ef7d9d58029184
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-03 20:33:09 +0000

    Cleaner

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,7 +1,7 @@
 from parse import parse
 
-f = [p for n,x,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if n <= p.count(l) <= x]
+f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if a <= p.count(l) <= b]
 print(len(f))
 
-f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if (p[a-1]==l and p[b-1]!=l) or (p[a-1]!=l and p[b-1]==l)]
+f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if p[a-1]!=p[b-1] and (p[a-1]==l or p[b-1]==l)]
 print(len(f))
