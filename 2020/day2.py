46c619482f985c5f3325a69ae732c359678f2f19
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-03 16:06:57 +0000

    Day 2 and 3

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,10 +1,7 @@
 from parse import parse
 
-*f, = filter(lambda nxlp: nxlp[0] <= nxlp[3].count(nxlp[2]) <= nxlp[1], map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()))
+f = [p for n,x,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if n <= p.count(l) <= x]
 print(len(f))
 
-c = 0
-for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()):
-  if (p[a-1]==l and p[b-1]!=l) or (p[a-1]!=l and p[b-1]==l):
-    c += 1
-print(c)
+f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("day2.txt").readlines()) if (p[a-1]==l and p[b-1]!=l) or (p[a-1]!=l and p[b-1]==l)]
+print(len(f))
--- 2020/day3.py
+++ 2020/day3.py
@@ -1,14 +1,12 @@
 m=[]
-for line in open("day3.txt").readlines():
-  m.append([0 if c=="." else 1 for c in line.strip()])
-for i,l in enumerate(m):
-  m[i] = l*128
+for line in open("day3.txt").readlines(): m.append([0 if c=="." else 1 for c in line.strip()])
 T=1
 for r,d in [(1,1),(3,1),(5,1),(7,1),(1,2)]:
   x,y=0,0
   t=0
   while y < len(m):
     t+=m[y][x]
-    x,y = x+r,y+d
+    x,y = (x+r)%31,y+d
   T *= t
+  if (r,d)==(3,1): print(t)
 print(T)
