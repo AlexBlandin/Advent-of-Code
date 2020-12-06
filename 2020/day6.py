11e7624f252d836746ab6eb004b5e8cf86105f3d
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-06 12:14:58 +0000

    Rolled D6

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,2 +1,1 @@
-from functools import reduce
 print(sum([len(set(g)-set("\n")) for g in open("data/day6.txt").read().split("\n\n")]), sum([len(g[0].intersection(*g[1:])) for g in [list(map(set,g.splitlines())) for g in open("data/day6.txt").read().split("\n\n")]]))
