3ad1a2291c8d409946998416fb3dcf0a6e2b417c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-06 01:52:07 +0000

    Commit 1linery

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,17 +1,15 @@
-*l,=map(int, open("data/day1.txt").readlines())
-i={}
-for x in l:
-  if x not in i:
-    i[2020-x]=x
-  else:
-    print(f"{x = }, {i[x] = }, {x + i[x] = }, {x * i[x] = }")
-    break
+# *l,=map(int, open("data/day1.txt").readlines())
+# i={}
+# for x in l:
+  # if x not in i:
+    # i[2020-x]=x
+  # else:
+    # print(x * i[x])
+    # break
 
 from itertools import combinations
 from functools import reduce
 from operator import mul
-print(list(filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(l,3))))[0][1])
+# print(list(filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(l,3))))[0][1])
 
-print()
-print("Now for the obligatory oneliner")
-[print(list(map(lambda x:x[1],filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(map(int, open("data/day1.txt").readlines()),n)))))[0]) for n in (2,3)]
+print(*[list(map(lambda x:x[1],filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(map(int, open("data/day1.txt").readlines()),n)))))[0] for n in (2,3)])
--- 2020/day2.py
+++ 2020/day2.py
@@ -1,7 +1,3 @@
 from parse import parse
-
-f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("data/day2.txt").readlines()) if a <= p.count(l) <= b]
-print(len(f))
-
-f = [p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("data/day2.txt").readlines()) if (p[a-1]==l) != (p[b-1]==l)]
-print(len(f))
+print(len([p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("data/day2.txt").readlines()) if a <= p.count(l) <= b]),\
+      len([p for a,b,l,p in map(lambda l: parse("{:d}-{:d} {:l}: {}", l).fixed, open("data/day2.txt").readlines()) if (p[a-1]==l) != (p[b-1]==l)]))
