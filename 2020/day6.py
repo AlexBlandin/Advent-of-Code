7a6f03c2498226b879eea5f885bd315fcb4e3be6
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-06 14:06:30 +0000

    The kwzrd patch

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,1 +1,1 @@
-print(sum([len(set(g)-set("\n")) for g in open("data/day6.txt").read().split("\n\n")]), sum([len(g[0].intersection(*g[1:])) for g in [list(map(set,g.splitlines())) for g in open("data/day6.txt").read().split("\n\n")]]))
+print(sum([len(set(g)-set("\n")) for g in open("data/day6.txt").read().split("\n\n")]), sum([len(set.intersection(*g)) for g in [list(map(set,g.splitlines())) for g in open("data/day6.txt").read().split("\n\n")]]))
