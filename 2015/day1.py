9ae1efc9ab0898792e7c38a368e8b1d8a52034da
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-06 01:40:51 +0000

    Day 1 2015 done late

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,1 +1,2 @@
-with open("data/day1.txt") as o: print((s:=o.read()).count("(")-s.count(")"))
+from itertools import accumulate
+with open("data/day1.txt") as o: print((s:=str(o.read())).count("(")-s.count(")"), list(accumulate([1 if c == "(" else -1 for c in s if c in "()"])).index(-1)+1)
