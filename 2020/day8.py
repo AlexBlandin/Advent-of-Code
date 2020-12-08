c9d208ec08499f8cb2daf8c544d2174303b3b4f0
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-08 15:13:32 +0000

    Tuples are nice

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,28 +1,28 @@
-with open("data/day8.txt") as o: b=[l.strip().split()+[True] for l in o.readlines()]
+with open("data/day8.txt") as o: b=[tuple(l.strip().split()) for l in o.readlines()]
 h=set()
 a,c=0,0
 while c not in h and c<len(b):
-  i=b[c]
+  i,v=b[c]
   h.add(c)
   c+=1
-  if i[0]=="acc":
-    a+=int(i[1])
-  elif i[0]=="jmp":
-    c+=int(i[1])-1
+  if i=="acc":
+    a+=int(v)
+  elif i=="jmp":
+    c+=int(v)-1
 p=a
 for s, op in enumerate(b):
   if op[0] in ["jmp", "nop"]:
-    pb,b[s] = b[s],"jmp" if b[s]=="nop" else "nop"
+    pb,b[s] = b[s],("jmp" if b[s]=="nop" else "nop",b[s][1])
     h=set()
     a,c=0,0
     while c not in h and c<len(b):
-      i=b[c]
+      i,v=b[c]
       h.add(c)
       c+=1
-      if i[0]=="acc":
-        a+=int(i[1])
-      elif i[0]=="jmp":
-        c+=int(i[1])-1
+      if i=="acc":
+        a+=int(v)
+      elif i=="jmp":
+        c+=int(v)-1
     b[s]=pb
     if c >= len(b):
       print(p,a)
