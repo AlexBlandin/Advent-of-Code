b6c4bef006d62dcb7efd6c31f3b7e624783f6feb
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-10 23:01:54 +0000

    Cleanup

--- 2020/day10.py
+++ 2020/day10.py
@@ -1,11 +1,8 @@
-from itertools import product
-from functools import lru_cache
-
-G, a = {}, [0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])
+a = [0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])
 a += [a[-1]+3]
-b, g, G = [y-x for x,y in zip(a,a[1:])], [(x,y) for x,y in product(a,a) if 1<=(y-x)<=3], {x:[] for x in a}
-for x,y in g: G[x].append(y)
+b, G = [y-x for x,y in zip(a,a[1:])], {x:[y for y in range(x+1,x+4) if y in a] for x in a}
 
+from functools import lru_cache
 @lru_cache()
 def rec(x): return 1 if x==161 else sum(rec(y) for y in G[x])
 print(b.count(1)*b.count(3), rec(0))
