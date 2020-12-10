cf98da007f044e2f4dc13b6f9b46c742fdf3b909
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-10 22:59:49 +0000

    Cleanup

--- 2020/day10.py
+++ 2020/day10.py
@@ -3,8 +3,8 @@ from functools import lru_cache
 
 G, a = {}, [0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])
 a += [a[-1]+3]
-b, g = [y-x for x,y in zip(a,a[1:])], [(x,y) for x,y in product(a,a) if 1<=(y-x)<=3]
-for x,y in g: G.setdefault(x,[]).append(y)
+b, g, G = [y-x for x,y in zip(a,a[1:])], [(x,y) for x,y in product(a,a) if 1<=(y-x)<=3], {x:[] for x in a}
+for x,y in g: G[x].append(y)
 
 @lru_cache()
 def rec(x): return 1 if x==161 else sum(rec(y) for y in G[x])
