fc28cdb7d2c1c881e308da49a30ccc961e98dc22
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-10 19:57:30 +0000

    Belated Day 2 2015

--- 2015/day2.py
+++ 2015/day2.py
@@ -1,1 +1,5 @@
-[tuple(map(int,l.split("x"))) for l in open("data/day2.txt")]
+from itertools import combinations
+from functools import reduce
+from operator import mul
+vs=[list(map(int,l.split("x"))) for l in open("data/day2.txt")]
+print(sum(2*sum(a*b for a,b in combinations(v,2))+min(a*b for a,b in combinations(v,2)) for v in vs), sum(min(list(a+a+b+b for a,b in combinations(v,2)))+reduce(mul,v) for v in vs))
