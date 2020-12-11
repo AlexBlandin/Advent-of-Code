d4159e5a9084e60a67efe45ff59cd6e24bdbab4e
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-11 12:36:13 +0000

    Day 11 begins

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,1 +1,2 @@
-open("data/day11.txt")
+m=[[None if c == "." else False for c in line.strip()] for line in open("data/day11.txt").readlines()]
+for l in m: print("".join("." if c==None else "#" if c else "L" for c in l))
