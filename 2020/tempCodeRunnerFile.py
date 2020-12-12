34c44d5da85096428a349eef4359dcca1aa0e20f
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-12 12:01:32 +0000

    Day12 underway

--- 2020/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-while m!=p:
-#   p=deepcopy(m)
-#   for y,row in enumerate(m):
-#     for x,s in enumerate(row):
-#       if s==False and sees(x,y).count(True)==0:
-#         m[y][x]=True
-#       elif s and sees(x,y).count(True)>=5:
-#         m[y][x]=False
