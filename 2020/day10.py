e1be45133aa862f3e7f9447c4abc4d6225ecf89b
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-12 07:46:38 +0000

    Added one-liner

--- 2020/day10.py
+++ 2020/day10.py
@@ -6,3 +6,7 @@ from functools import lru_cache
 @lru_cache
 def rec(x): return 1 if x==a[-1] else sum(rec(y) for y in G[x])
 print(b.count(1)*b.count(3), rec(0))
+
+# from functools import lru_cache
+# _,__,b,G,r = (a:=[0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])),(a:=a + [a[-1]+3]),[y-x for x,y in zip(a,a[1:])], {x:[y for y in range(x+1,x+4) if y in a] for x in a},lru_cache(lambda x: 1 if x==a[-1] else sum(r(y) for y in G[x]))
+# print(b.count(1)*b.count(3), r(0))
