7213257b9aa782293daf275fa2f297abcd061cc0
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-13 21:00:49 +0000

    Pypy timing

--- 2020/day13.py
+++ 2020/day13.py
@@ -7,6 +7,7 @@ contest = next(T for T in range(999999999999222,10**14, -mx) if all(T%b==o for o
 print((min(bus)[0]-e)*min(bus)[1], contest)
 # should take ~4 mins at magic 1op 4GHz (1047729918510 T in range(10**14,10**15,859), ~10**12 / 4*10**9 is ~250s)
 # back of napkin for C is avg. ~3op per, took 1m44s, this should take proportionally ~10x-100x as long
+# pypy takes 82m14.731s within expectations
 
 # print(", ".join(f"n%{b} == {o}" for o,b in mes)) # to plug into WA
 # n%19 == 0, n%41 == 32, n%859 == 840, n%23 == 19, n%13 == 7, n%17 == 15, n%29 == 10, n%373 == 323, n%37 == 24
