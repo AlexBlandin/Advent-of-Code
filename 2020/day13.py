f1b888ab3f664ccce4880030f9cfe0f220a0a6b6
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-13 16:34:05 +0000

    Better brute force

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,8 +1,9 @@
 from math import ceil
 from itertools import count
-estimate,schedule=tuple(map(str.strip,open("data/day13.txt").readlines()))
-estimate,schedule=int(estimate), [int(i) if i!="x" else None for i in schedule.split(",")]
-times=[(ceil(estimate/i)*i,i) for i in schedule if i]
-es = [(o,b) for o,b in enumerate(schedule) if b]
-contest=next((T for T in range(ceil(10**14/schedule[0])*schedule[0],10**16, schedule[0]) if all((T+o)%b==0 for o,b in es)))
-print((min(times)[0]-estimate)*min(times)[1], contest)
+e,s = tuple(map(str.strip,open("data/day13.txt").readlines()))
+e,s = int(e), [int(i) if i!="x" else None for i in s.split(",")]
+bus = [(ceil(e/i)*i,i) for i in s if i]
+mes = [((b-o)%b, b) for o,b in enumerate(s) if b]
+contest = next((T for T in range(ceil(10**14/s[0])*s[0],10**16, s[0]) if all(T%b==o for o,b in mes)))
+# ((T+o)%b == 0) === (T%b == (o:=(b-o)%b))
+print((min(bus)[0]-e)*min(bus)[1], contest)
