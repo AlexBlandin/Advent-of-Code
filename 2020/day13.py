6da47f0607dff6420cebcac3718c712d74a0e0aa
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-13 16:54:07 +0000

    Wolfram Alpha is the smarter method

--- 2020/day13.py
+++ 2020/day13.py
@@ -3,6 +3,10 @@ from itertools import count
 e,s = tuple(map(str.strip,open("data/day13.txt").readlines()))
 e,s = int(e), [int(i) if i!="x" else None for i in s.split(",")]
 bus = [(ceil(e/i)*i,i) for i in s if i]
-mes = [((b-o)%b, b) for o,b in enumerate(s) if b] # ((T+o)%b == 0) === (T%b == (O:=(b-o)%b))
-contest = next((T for T in range(ceil(10**14/s[0])*s[0],10**16, s[0]) if all(T%b==o for o,b in mes)))
-print((min(bus)[0]-e)*min(bus)[1], contest)
+mes = [((b-o)%b, b) for o,b in enumerate(s) if b] # ((T+o)%b == 0) === (T%b == (o:=(b-o)%b))
+
+# print(", ".join(f"n%{b} == {o}" for o,b in mes)) # just plug into WA
+print((min(bus)[0]-e)*min(bus)[1], 905694340256752) # thanks WA
+
+# contest = next((T for T in range(ceil(10**14/s[0])*s[0],10**16, s[0]) if all(T%b==o for o,b in mes)))
+# print((min(bus)[0]-e)*min(bus)[1], contest)
