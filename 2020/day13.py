5b88d099440e7df8fac55a1743a97df73d03d78d
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-13 15:20:03 +0000

    Day 13 brute forcer, coming up with something smarter

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,1 +1,8 @@
-open("data/day13.txt")
+from math import ceil
+from itertools import count
+estimate,schedule=tuple(map(str.strip,open("data/day13.txt").readlines()))
+estimate,schedule=int(estimate), [int(i) if i!="x" else None for i in schedule.split(",")]
+times=[(ceil(estimate/i)*i,i) for i in schedule if i]
+es = [(o,b) for o,b in enumerate(schedule) if b]
+contest=next((T for T in range(10**14,10**16) if all((T+o)%b==0 for o,b in es)))
+print((min(times)[0]-estimate)*min(times)[1], contest)
