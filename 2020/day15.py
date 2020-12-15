9dd852eb1755398a8df85c56f09c1bfa5c9bb5e9
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-15 13:09:26 +0000

    Day 15 done

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,1 +1,14 @@
-[19,0,5,1,10,13]
+from collections import deque
+m=[19,0,5,1,10,13] # [0,3,6] or
+rm,sm,im=deque(reversed(m)),set(m[:-1]),{n:i for i,n in enumerate(m,1)}
+for i in range(len(m),30000000): # 2020 or # 10 or
+  l=m[-1]
+  if l not in sm:
+    m+=[0]
+    sm.add(l)
+    im[l]=i
+  else:
+    a=i-im[l]
+    im[l]=i
+    m+=[a]
+print(m[-1])
