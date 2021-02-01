739bfa79294ae1520184d907e1e5e68c5e8dc512
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-02-01 13:09:15 +0000

    Day 16, basically just finally got around to doing the obvious thing

--- 2020/day16.py
+++ 2020/day16.py
@@ -8,19 +8,21 @@ except:
   def prod(iterable, start=1): return reduce(mul, iterable, initial=start) # not 3.9 (ie, pypy)
 samples = lambda s,k: sample(sorted(s),k) # 3.9 why you do this to me?
 
-with open("data/day16-1.txt") as o:
+with open("data/day16.txt") as o:
   lines = [line.strip() for line in o.readlines()]
 
 mine = [int(n) for n in lines[lines.index("your ticket:")+1].split(",")]
 scanned = [[int(n) for n in line.split(",")] for line in lines[lines.index("nearby tickets:")+1:]]
+field_count = len(scanned[0])
 inverse, rules = {},{}
 for rule in findall("{}: {:d}-{:d} or {:d}-{:d}", " ".join(lines[:lines.index("your ticket:")-1])):
   field,a,b,x,y=rule.fixed
-  field=field.strip()
+  field = field.strip()
   rules[field] = (range(a,b+1),range(x,y+1))
-  for n in list(range(a,b+1))+list(range(x,y+1)): inverse.setdefault(n,[]).append(field)
-inverse = {k:set(v) for k,v in inverse.items()}
+  for n in list(range(a,b+1))+list(range(x,y+1)): inverse.setdefault(n,set()).add(field)
+# inverse = {k:set(v) for k,v in inverse.items()}
 ruleset = set(rules.keys())
+assert(len(ruleset)==field_count)
 
 errors = [value for ticket in scanned for value in ticket if value not in inverse]
 scanned = [ticket for ticket in scanned if all(value in inverse for value in ticket)]
@@ -29,15 +31,19 @@ multiple = {k:v for k,v in inverse.items() if len(v)>1}
 single = {k:v for k,v in inverse.items() if len(v)==1}
 
 # ideally, we can do THIS but in some ever decreasing manner, with forced moves cascading to a (the?) solution
-fields = [set.union(*[inverse[field] for field in [ticket[i] for ticket in scanned]]) for i in range(len(scanned[0]))]
-
-# at least 19 rules per field... so they could be anything x_x
-# print(min(min([len(inverse[field]) for field in ticket) for ticket in scanned]))
+fields = [set.intersection(*[inverse[field] for field in [ticket[i] for ticket in scanned]]) for i in range(field_count)]
 
 # do without full SAT solver, just by DPLL elimination
-per = [[inverse[field] for field in ticket] for ticket in scanned]
-
-
-
-# print(sum(errors), prod(departure))
-print(sum(errors))
+solved = [None]*field_count
+while max(map(len,fields)):
+  tomb = set()
+  for i,candidates in enumerate(fields):
+    if len(candidates)==1:
+      solved[i]=candidates.pop()
+      tomb.add(solved[i])
+  for candidates in fields:
+    candidates -= tomb
+departure = [mine[i] for i,rule in enumerate(solved) if rule.startswith("departure")]
+# print(solved)
+# print(departure)
+print(sum(errors), prod(departure))
