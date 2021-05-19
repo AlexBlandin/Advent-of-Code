816bb2c5336e033868b51c67b5771474b1b434d6
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2021-05-20 00:10:12 +0100

    Generalised neighbours

--- 2020/day17.py
+++ 2020/day17.py
@@ -8,21 +8,17 @@ with open("data/day17.txt") as o:
         D.add((x,y,0))
         H.add((x,y,0,0))
 
-def neighbours3d(p):
-  x,y,z = p
-  return [(x+dx,y+dy,z+dz) for dx,dy,dz in product((0,-1,1), repeat=3)][1:] # we don't return p
-def neighbours4d(p):
-  x,y,z,w = p
-  return [(x+dx,y+dy,z+dz,w+dw) for dx,dy,dz,dw in product((0,-1,1), repeat=4)][1:]
+def neighbours(p, dim):
+  return [tuple(a+b for a,b in zip(p,c)) for c in product((0,-1,1), repeat=dim)][1:]
 
 timesteps = 6
 for t in range(timesteps):
   d, h = {}, {} # all adjacencies points from D|H, and the value is the number of adjacencies
   for p in D:
-    for n in neighbours3d(p):
+    for n in neighbours(p, 3):
       d[n] = d.get(n, 0)+1
   for p in H:
-    for n in neighbours4d(p):
+    for n in neighbours(p, 4):
       h[n] = h.get(n, 0)+1
   D = {p for p, v in d.items() if v==3 or (p in D and v==2)} # filter to only the "on" points
   H = {p for p, v in h.items() if v==3 or (p in H and v==2)}
