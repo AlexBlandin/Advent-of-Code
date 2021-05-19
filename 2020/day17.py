79adab365762ddbaadac8c082043748028514350
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2021-05-20 00:01:37 +0100

    Comments

--- 2020/day17.py
+++ 2020/day17.py
@@ -10,21 +10,21 @@ with open("data/day17.txt") as o:
 
 def neighbours3d(p):
   x,y,z = p
-  return [(x+dx,y+dy,z+dz) for dx,dy,dz in product((0,-1,1), (0,-1,1), (0,-1,1))][1:] # don't return p, so drop first (hence why the product is that way, faster than IF)
+  return [(x+dx,y+dy,z+dz) for dx,dy,dz in product((0,-1,1), (0,-1,1), (0,-1,1))][1:] # we don't return p
 def neighbours4d(p):
   x,y,z,w = p
   return [(x+dx,y+dy,z+dz,w+dw) for dx,dy,dz,dw in product((0,-1,1), (0,-1,1), (0,-1,1), (0,-1,1))][1:]
 
 timesteps = 6
 for t in range(timesteps):
-  d, h = {}, {} # our partial dict, we handle off points as adjacencies to points from D (so v>0), and the value is the number of adjacencies (active and v!=2|3 -> inactive, inactive and v==3 -> active)
+  d, h = {}, {} # all adjacencies points from D|H, and the value is the number of adjacencies
   for p in D:
     for n in neighbours3d(p):
       d[n] = d.get(n, 0)+1
   for p in H:
     for n in neighbours4d(p):
       h[n] = h.get(n, 0)+1
-  D = {p for p, v in d.items() if v==3 or (p in D and v==2)}
+  D = {p for p, v in d.items() if v==3 or (p in D and v==2)} # filter to only the "on" points
   H = {p for p, v in h.items() if v==3 or (p in H and v==2)}
 
-print(len(D), len(H))
+print(len(D), len(H)) # and we're done!
