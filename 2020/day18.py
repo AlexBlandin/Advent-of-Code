f8c38aa18e987fec36d8bcee49afe3ab256afee8
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2021-05-23 14:36:53 +0100

    Stack machine for part 1

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,18 +1,39 @@
-from parse import *
 from operator import add, mul
 
 with open("data/day18.txt") as o:
   lines = [line.strip() for line in o]
 
-@with_pattern(r"[\*\+]")
-def parse_op(text):
-  return {"*": mul, "+": add}[text]
+class dot(dict): # as in a "dot dict", a dict you can access by a "."
+  __getattr__, __setattr__ = dict.__getitem__, dict.__setitem__
 
-results, log = [], []
+results = []
 for line in lines:
-  
-  simple = findall("{a:d} {op:Op} {b:d}", line, {"Op":parse_op}) # they have their .named values and .spans (lets us navigate)
-  parens = findall("{a:d} {op:Op} (", line, {"Op":parse_op})
-  
-  results.append()
-  log.append((,))
+  stack, acc, op, set_op = [], 0, add, False
+  # stack grows on "(", pushing (acc,op) pair, pops on ")" and sets acc = top.op(top.acc,pop.acc) then pops the old (acc,op) so we can add on next "("
+  # if starts "(" we push (0,add) and so carry forward acc on subsequent ")" (basically it's pretending to be a no-op preserving acc)
+  # print(line)
+  for section in line.split():
+    # print(section,end=" ")
+    while section[0]=="(":
+      section=section[1:]
+      stack.append(dot(acc=acc,op=op))
+      acc,op=0,add # so we can have multiple nestings
+    to_pop = section.count(")")
+    if to_pop > 0: section=section[:-to_pop]
+    if section in "*+": 
+      op = {"*":mul,"+":add}[section]
+      set_op=True
+    elif set_op:
+      acc=op(acc,int(section))
+      set_op=False
+    else:
+      acc=int(section)
+    while to_pop > 0:
+      to_pop -= 1
+      top = stack.pop()
+      acc = top.op(top.acc, acc)
+    # print(acc, op, set_op, stack)
+  # print(f"{line} = {acc}")
+  results.append(acc)
+
+print(sum(results))
