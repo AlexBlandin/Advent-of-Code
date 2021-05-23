ae0333839bf6a513468e1fe3e541b70e65a4548b
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2021-05-23 13:48:43 +0100

    Try this instead

---
+++ 2020/day18-1.py
@@ -0,0 +1,26 @@
+with open("data/day18.txt") as o:
+  lines = [line.strip() for line in o]
+
+results, log = [], []
+for line in lines:
+  
+  # what I want to detect is that "you have mixed + and * within the same brackets / at top-level" and only do "bracketing" then
+  # this currently inserts it "dumbly"
+  
+  result = []
+  for nex, val, prev in zip(line.split()[1:]+[None], line.split(), [None]+line.split()):
+    if val[0].isdigit() and nex in ["+", "*"] and prev in ["+", "*"]:
+      result.append(f"{val})")
+    elif val[0].isdigit() and nex in ["+", "*"]:
+      result.append(f"({val}") # replace "<val>" with "(<val>"
+    elif val[0].isdigit() and prev in ["+", "*"] and val[-1] != ")":
+      result.append(f"{val})") # replace "<val>" with "<val>)"
+    else:
+      result.append(val)
+  result = " ".join(result)
+  result = "("*(result.count(")")-result.count("("))+result+")"*(result.count("(")-result.count(")"))
+  results.append(eval(result)) # so a nice idea, wrong results
+  log.append((result, eval(result)))
+
+print(log[:3])
+print(sum(results))
--- 2020/day18.py
+++ 2020/day18.py
@@ -1,26 +1,18 @@
+from parse import *
+from operator import add, mul
+
 with open("data/day18.txt") as o:
   lines = [line.strip() for line in o]
+
+@with_pattern(r"[\*\+]")
+def parse_op(text):
+  return {"*": mul, "+": add}[text]
 
 results, log = [], []
 for line in lines:
   
-  # what I want to detect is that "you have mixed + and * within the same brackets / at top-level" and only do "bracketing" then
-  # this currently inserts it "dumbly"
+  simple = findall("{a:d} {op:Op} {b:d}", line, {"Op":parse_op}) # they have their .named values and .spans (lets us navigate)
+  parens = findall("{a:d} {op:Op} (", line, {"Op":parse_op})
   
-  result = []
-  for nex, val, prev in zip(line.split()[1:]+[None], line.split(), [None]+line.split()):
-    if val[0].isdigit() and nex in ["+", "*"] and prev in ["+", "*"]:
-      result.append(f"{val})")
-    elif val[0].isdigit() and nex in ["+", "*"]:
-      result.append(f"({val}") # replace "<val>" with "(<val>"
-    elif val[0].isdigit() and prev in ["+", "*"] and val[-1] != ")":
-      result.append(f"{val})") # replace "<val>" with "<val>)"
-    else:
-      result.append(val)
-  result = " ".join(result)
-  result = "("*(result.count(")")-result.count("("))+result+")"*(result.count("(")-result.count(")"))
-  results.append(eval(result)) # so a nice idea, wrong results
-  log.append((result, eval(result)))
-
-print(log[:3])
-print(sum(results))
+  results.append()
+  log.append((,))
