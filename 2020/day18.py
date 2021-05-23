943054d7380abbe8d2947cd4a104f5de2823ad14
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2021-05-23 15:09:50 +0100

    Day 18 done

--- 2020/day18.py
+++ 2020/day18.py
@@ -3,10 +3,13 @@ from operator import add, mul
 with open("data/day18.txt") as o:
   lines = [line.strip() for line in o]
 
+# Examples
+# lines = ["1 + (2 * 3) + (4 * (5 + 6))","2 * 3 + (4 * 5)","5 + (8 * 3 + 9 + 3 * 4 * 3)", "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
+
 class dot(dict): # as in a "dot dict", a dict you can access by a "."
   __getattr__, __setattr__ = dict.__getitem__, dict.__setitem__
 
-results = []
+results1 = []
 for line in lines:
   stack, acc, op, set_op = [], 0, add, False
   # stack grows on "(", pushing (acc,op) pair, pops on ")" and sets acc = top.op(top.acc,pop.acc) then pops the old (acc,op) so we can add on next "("
@@ -34,6 +37,26 @@ for line in lines:
       acc = top.op(top.acc, acc)
     # print(acc, op, set_op, stack)
   # print(f"{line} = {acc}")
-  results.append(acc)
+  results1.append(acc)
 
-print(sum(results))
+from dataclasses import dataclass
+results2 = []
+log=[]
+@dataclass
+class Moon:
+  x: int
+  def __add__(self, o):  return Moon(self.x*o.x)# if isinstance(o, Moon) else Moon(self.x*o)
+  def __radd__(self, o): return Moon(self.x*o.x)# if isinstance(o, Moon) else Moon(self.x*o)
+  def __mul__(self, o):  return Moon(self.x+o.x)# if isinstance(o, Moon) else Moon(self.x+o)
+  def __rmul__(self, o): return Moon(self.x+o.x)# if isinstance(o, Moon) else Moon(self.x+o)
+
+for line in lines:
+  log.append(line)
+  for i in range(1,10): line = line.replace(f"{i}", f"Moon({i})")
+  line = line.replace("*","-").replace("+","*").replace("-","+")
+  res = eval(line, globals()).x
+  log[-1]=f"{log[-1]} -> {line} = {res}"
+  results2.append(res)
+
+# print("\n".join(log[:5]))
+print(sum(results1), sum(results2))
