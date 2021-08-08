bcffcb2936afe681dbcdf2d8c4a30570bfe95789
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-08-08 16:49:56 +0100

    Cleanup

--- 2020/day1.py
+++ 2020/day1.py
@@ -8,8 +8,13 @@
     # break
 
 from itertools import combinations
-from functools import reduce
-from operator import mul
-# print(list(filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(l,3))))[0][1])
+from operator import itemgetter
+try:
+  from math import prod # 3.9
+except:
+  from functools import reduce
+  from operator import mul
+  def prod(iterable, start=1): return reduce(mul, iterable, initial=start) # not 3.9 (ie, pypy)
+# print(list(filter(lambda x:x[0]==2020, map(lambda x: (sum(x),prod(x)),combinations(l,3))))[0][1])
 
-print(*[list(map(lambda x:x[1],filter(lambda x:x[0]==2020, map(lambda x: (sum(x),reduce(mul,x)),combinations(map(int, open("data/day1.txt").readlines()),n)))))[0] for n in (2,3)])
+print(*[list(map(itemgetter(1),filter(lambda x:x[0]==2020, map(lambda x: (sum(x),prod(x)),combinations(map(int, open("data/day1.txt").readlines()),n)))))[0] for n in (2,3)])
--- 2020/day13.py
+++ 2020/day13.py
@@ -1,8 +1,9 @@
+from operator import itemgetter
 from math import ceil
 e,s = tuple(map(str.strip,open("data/day13.txt").readlines()))
 e,s = int(e), [int(i) if i!="x" else None for i in s.split(",")]
 bus = [(ceil(e/i)*i,i) for i in s if i]
-mes, (mo,mx) = [((b-o)%b, b) for o,b in enumerate(s) if b], max([t for t in enumerate(s) if t[1]], key=lambda t:t[1])
+mes, (mo,mx) = [((b-o)%b, b) for o,b in enumerate(s) if b], max([t for t in enumerate(s) if t[1]], key=itemgetter(1))
 contest = next(T for T in range(999999999999222,10**14, -mx) if all(T%b==o for o,b in mes))
 print((min(bus)[0]-e)*min(bus)[1], contest)
 # should take ~4 mins at magic 1op 4GHz (1047729918510 T in range(10**14,10**15,859), ~10**12 / 4*10**9 is ~250s)
