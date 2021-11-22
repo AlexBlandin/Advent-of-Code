b1d488480619d2f3fe6131066d25fab0c97674ee
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-22 08:57:45 +0000

    Cleanup

--- 2015/day7.py
+++ 2015/day7.py
@@ -1,52 +1,51 @@
+from operator import inv, or_, and_, lshift, rshift
 from graphlib import TopologicalSorter as topo
+from collections import defaultdict
 from parse import parse
 with open("data/day7.txt") as o:
   lines = [line.strip() for line in o.readlines()]
 
-G = {} # G[w] = {s0,s1} # wire sources
+G = defaultdict(set) # G[w] = {s0,s1} # wire sources
 O = {} # O[w] = op() # op() lazily perform lookups for "current" value
-A = {} # A[w] = uint16 # the value of a wire, initialised to 0
+A = {} # the value of a wire
 
 def add_input(G, A, w, s):
-  A[w] = 0
-  if s.isdecimal(): A[s] = int(s) # a constant
-  else:
-    if w not in G: G[w] = set()
-    G[w].add(s)
+  if s.isdecimal(): A[s] = int(s)
+  else: G[w].add(s)
+
+def op(f, *arg):
+  return f(*[A[a] for a in arg]) & 65535
 
 for line in lines:
-  if p := parse("NOT {} -> {}", line):
-    s0, w = p.fixed
-    O[w] = s0, lambda s: ~A[s]
-  elif p := parse("{} AND {} -> {}", line):
-    s0, s1, w = p.fixed
-    add_input(G, A, w, s1)
-    O[w] = (s0, s1), lambda s: A[s[0]] & A[s[1]]
-  elif p := parse("{} OR {} -> {}", line):
-    s0, s1, w = p.fixed
-    add_input(G, A, w, s1)
-    O[w] = (s0, s1), lambda s: A[s[0]] | A[s[1]]
-  elif p := parse("{} LSHIFT {} -> {}", line):
-    s0, s1, w = p.fixed
-    add_input(G, A, w, s1)
-    O[w] = (s0,s1), lambda s: A[s[0]]<<A[s[1]] & 65535
-  elif p := parse("{} RSHIFT {} -> {}", line):
-    s0, s1, w = p.fixed
-    add_input(G, A, w, s1)
-    O[w] = (s0,s1), lambda s: A[s[0]]>>A[s[1]]
-  elif p := parse("{} -> {}", line):
-    s0, w = p.fixed
-    O[w] = s0, lambda s: A[s]
-  add_input(G, A, w, s0)
+  lhs, w = line.split(" -> ")
+  if p := parse("NOT {}", lhs):
+    s0 = p.fixed[0]
+    O[w] = inv, s0
+  elif p := parse("{} AND {}", lhs):
+    s0, s1 = p.fixed
+    O[w] = and_, s0, s1
+  elif p := parse("{} OR {}", lhs):
+    s0, s1 = p.fixed
+    O[w] = or_, s0, s1
+  elif p := parse("{} LSHIFT {}", lhs):
+    s0, s1 = p.fixed
+    O[w] = lshift, s0, s1
+  elif p := parse("{} RSHIFT {}", lhs):
+    s0, s1 = p.fixed
+    O[w] = rshift, s0, s1
+  elif p := parse("{}", lhs):
+    s0 = p.fixed[0]
+    O[w] = lambda s: s, s0
+  for s in p: add_input(G, A, w, s)
 
 order = tuple(topo(G).static_order())
-for e, (s, o) in zip(order, map(O.get, order)):
-  A[e] = o(s)
+for e, (o, *s) in zip(order, map(O.get, order)):
+  A[e] = op(o, *s)
 
 old_a = A["a"] # previous output
-O["b"] = old_a, lambda s: old_a
+O["b"], A[old_a] = (lambda s: s, old_a), old_a
 
-for e, (s, o) in zip(order, map(O.get, order)):
-  A[e] = o(s)
+for e, (o, *s) in zip(order, map(O.get, order)):
+  A[e] = op(o, *s)
 
 print(old_a, A["a"])
