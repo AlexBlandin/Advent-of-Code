321d91f0a178772afb86900aeb7741f199b78745
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-22 02:34:05 +0000

    2015 Day 4

--- 2015/day4.py
+++ 2015/day4.py
@@ -1,1 +1,14 @@
-open("data/day4.txt")
+from hashlib import md5
+
+secret = "bgvyzdsv"
+five, six = 0, 0
+for i in range(10**9):
+  if md5(bytes(f"bgvyzdsv{i}","utf8")).hexdigest()[:5]=="00000":
+    five = i
+    break
+for i in range(10**9):
+  if md5(bytes(f"bgvyzdsv{i}","utf8")).hexdigest()[:6]=="000000":
+    six = i
+    break
+
+print(five, six)
