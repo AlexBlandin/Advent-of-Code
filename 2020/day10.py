63f3e960e9ff3ef594273aa580e262a828a906ed
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-22 00:43:29 +0000

    Forgot to commit

--- 2020/day10.py
+++ 2020/day10.py
@@ -2,12 +2,16 @@ a = [0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])
 a += [a[-1]+3]
 b, G = [y-x for x,y in zip(a,a[1:])], {x:[y for y in range(x+1,x+4) if y in a] for x in a}
 
-from functools import lru_cache
-@lru_cache()
+try:
+  from functools import cache
+except: 
+  from functools import lru_cache as cache
+
+@cache
 def rec(x): return 1 if x==a[-1] else sum(rec(y) for y in G[x])
 print(b.count(1)*b.count(3), rec(0))
 
 # # Fear the one-liner
-# from functools import lru_cache
-# _,__,b,G,r = (a:=[0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])),(a:=a + [a[-1]+3]),[y-x for x,y in zip(a,a[1:])], {x:[y for y in range(x+1,x+4) if y in a] for x in a},lru_cache(lambda x: 1 if x==a[-1] else sum(r(y) for y in G[x]))
+# from functools import cache
+# _,__,b,G,r = (a:=[0]+sorted([int(l.strip()) for l in open("data/day10.txt").readlines()])),(a:=a + [a[-1]+3]),[y-x for x,y in zip(a,a[1:])], {x:[y for y in range(x+1,x+4) if y in a] for x in a},cache(lambda x: 1 if x==a[-1] else sum(r(y) for y in G[x]))
 # print(b.count(1)*b.count(3), r(0))
