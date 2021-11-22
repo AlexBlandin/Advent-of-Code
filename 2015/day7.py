4c288bc95eb5f08d2eaab5e05374ce9cf5e92b91
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-22 05:30:13 +0000

    Cleanup

--- 2015/day7.py
+++ 2015/day7.py
@@ -3,8 +3,8 @@ from parse import parse
 with open("data/day7.txt") as o:
   lines = [line.strip() for line in o.readlines()]
 
-G = {} # G[w] = {s0,s1,...}
-O = {} # O[w] = op(x) # ops lazily perform lookups for "current" value
+G = {} # G[w] = {s0,s1} # wire sources
+O = {} # O[w] = op() # op() lazily perform lookups for "current" value
 A = {} # A[w] = uint16 # the value of a wire, initialised to 0
 
 def add_input(G, A, w, s):
@@ -17,35 +17,33 @@ def add_input(G, A, w, s):
 for line in lines:
   if p := parse("NOT {} -> {}", line):
     s0, w = p.fixed
-    add_input(G, A, w, s0)
     O[w] = s0, lambda s: ~A[s]
   elif p := parse("{} AND {} -> {}", line):
     s0, s1, w = p.fixed
-    add_input(G, A, w, s0); add_input(G, A, w, s1)
+    add_input(G, A, w, s1)
     O[w] = (s0, s1), lambda s: A[s[0]] & A[s[1]]
   elif p := parse("{} OR {} -> {}", line):
     s0, s1, w = p.fixed
-    add_input(G, A, w, s0); add_input(G, A, w, s1)
+    add_input(G, A, w, s1)
     O[w] = (s0, s1), lambda s: A[s[0]] | A[s[1]]
   elif p := parse("{} LSHIFT {} -> {}", line):
     s0, s1, w = p.fixed
-    add_input(G, A, w, s0); add_input(G, A, w, s1)
+    add_input(G, A, w, s1)
     O[w] = (s0,s1), lambda s: A[s[0]]<<A[s[1]] & 65535
   elif p := parse("{} RSHIFT {} -> {}", line):
     s0, s1, w = p.fixed
-    add_input(G, A, w, s0); add_input(G, A, w, s1)
+    add_input(G, A, w, s1)
     O[w] = (s0,s1), lambda s: A[s[0]]>>A[s[1]]
   elif p := parse("{} -> {}", line):
     s0, w = p.fixed
-    add_input(G, A, w, s0)
     O[w] = s0, lambda s: A[s]
+  add_input(G, A, w, s0)
 
 order = tuple(topo(G).static_order())
 for e, (s, o) in zip(order, map(O.get, order)):
   A[e] = o(s)
 
 old_a = A["a"] # previous output
-for e in order: A[e] = 0 # reset
 O["b"] = old_a, lambda s: old_a
 
 for e, (s, o) in zip(order, map(O.get, order)):
