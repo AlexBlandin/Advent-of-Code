adbc98ae79ab7b0d3511fcff01e99e94787f8798
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-23 00:29:48 +0000

    2015 Day 8

--- 2015/day8.py
+++ 2015/day8.py
@@ -1,1 +1,9 @@
-open("data/day8.txt")
+from pathlib import Path
+
+lines = Path("data/day8.txt").read_text(encoding="latin1").splitlines()
+mem, esc, original = 0, 0, sum(map(len,lines))
+for i, line in enumerate(lines):
+  el, rl = eval(line), '"\\"'+repr(line)[2:-2].replace('"','\\"')+'\\""'
+  mem += len(el)
+  esc += len(rl)
+print(original - mem, esc - original)
