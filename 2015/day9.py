87be61cc29517427e9b78339f0378bf3fcc5ecac
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-24 20:41:12 +0000

    Cleanup

--- 2015/day9.py
+++ 2015/day9.py
@@ -1,35 +1,31 @@
-from collections import Counter, defaultdict
 from itertools import combinations, chain
+from collections import Counter
 from operator import itemgetter
 from pathlib import Path
 from parse import parse
 
 lines = Path("data/day9.txt").read_text().splitlines()
 
-G, D, C = defaultdict(set), {}, set()
+D, C = {}, set()
 
 for line in lines:
   if p := parse("{} to {} = {:d}", line):
     a,b,d = p.fixed
     a,b = min(a,b),max(a,b)
     D[(a,b)] = d
-    D[(b,a)] = d
-    G[a].add(b)
     C |= {a,b}
+D = dict(sorted(D.items(), key=itemgetter(0)))
 D = dict(sorted(D.items(), key=itemgetter(1)))
 
 def distance(c: tuple):
   return sum(map(D.get, c))
 
-def pair(G):
-  return {(a,b) for a,c in G.items() for b in c}
-
 def hamiltonian(c):
   d, s = list(Counter(chain(map(itemgetter(0),c),map(itemgetter(1),c))).values()), set(c)
   return d.count(1)+d.count(2)==len(d) and {a for b in c for a in b} == C and not any(True for a,b in c if (b,a) in s)
 
-mn = min(filter(hamiltonian, combinations(pair(G),len(C)-1)), key=distance)
-mx = max(filter(hamiltonian, combinations(pair(G),len(C)-1)), key=distance)
+mn = min(filter(hamiltonian, combinations(iter(D),len(C)-1)), key=distance)
+mx = max(filter(hamiltonian, combinations(iter(D),len(C)-1)), key=distance)
 print(distance(mn), distance(mx))
 # print({*map(" -> ".join, mn)})
 # print({*map(" -> ".join, mx)})
