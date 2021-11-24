136be10f345f0bc7febf95eaaa5bce053f5b6dca
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-24 02:15:00 +0000

    2015 Day 12

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,3 +1,25 @@
 from pathlib import Path
 lines = Path("data/day12.txt").read_text().splitlines()
+from json import loads
+db = loads(lines[0])
+def rsum(x):
+  if isinstance(x,int):
+    return x
+  elif isinstance(x, dict):
+    return sum(map(rsum,x.keys()))+sum(map(rsum,x.values()))
+  elif isinstance(x, list):
+    return sum(map(rsum,x))
+  else:
+    return 0
+def redsum(x):
+  if isinstance(x,int):
+    return x
+  elif isinstance(x, dict):
+    if "red" in x.values(): return 0 # that's literally it
+    return sum(map(redsum,x.keys()))+sum(map(redsum,x.values()))
+  elif isinstance(x, list):
+    return sum(map(redsum,x))
+  else:
+    return 0
 
+print(rsum(db), redsum(db))
