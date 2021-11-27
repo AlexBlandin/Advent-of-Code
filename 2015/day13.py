5be1c5852f5bbc78af4ae459a2fdb3a357ac6be7
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-27 17:22:10 +0000

    2015 Day 14

--- 2015/day13.py
+++ 2015/day13.py
@@ -34,3 +34,63 @@ with_me = max(filter(hamiltonian, combinations(iter(D),len(C))), key=delta)
 assert(delta(with_me) <= 755) # 258s on 8700k, 345s on 4700U # 755 is too high???
 
 print(delta(optimal), delta(with_me))
+
+print(sum({c:D[c] for c in optimal}.values()),{c:D[c] for c in optimal})
+print(sum({c:D[c] for c in with_me}.values()),{c:D[c] for c in with_me})
+
+{ ('George', 'Mallory'): 8,
+  ('David', 'Eric'): 33,
+  ('Alice', 'Bob'): 42,
+  ('David', 'George'): 94,
+  ('Carol', 'Frank'): 98,
+  ('Alice', 'Mallory'): 131,
+  ('Bob', 'Frank'): 132,
+  ('Carol', 'Eric'): 195
+}
+{ ('Alex', 'David'): 0,
+  ('Alex', 'George'): 0,
+  ('Alice', 'Bob'): 42,
+  ('Carol', 'Mallory'): 43,
+  ('David', 'George'): 94,
+  ('Eric', 'Frank'): 118,
+  ('Alice', 'Mallory'): 131,
+  ('Bob', 'Frank'): 132,
+  ('Carol', 'Eric'): 195
+}
+{ ('Frank', 'Mallory'): -165,
+  ('Bob', 'Carol'): -160,
+  ('Alice', 'Eric'): -140,
+  ('Carol', 'David'): -116,
+  ('David', 'Frank'): -88,
+  ('Alice', 'David'): -46,
+  ('David', 'Mallory'): -39,
+  ('Eric', 'George'): -37,
+  ('Bob', 'David'): -33,
+  ('Eric', 'Mallory'): -13,
+  ('Alice', 'Carol'): -9,
+  ('Bob', 'Mallory'): -9,
+  ('Alice', 'Frank'): -6,
+  ('Alice', 'George'): -6,
+  ('Alex', 'Alice'): 0,
+  ('Alex', 'Bob'): 0,
+  ('Alex', 'Carol'): 0,
+  ('Alex', 'David'): 0,
+  ('Alex', 'Eric'): 0,
+  ('Alex', 'Frank'): 0,
+  ('Alex', 'George'): 0,
+  ('Alex', 'Mallory'): 0,
+  ('Carol', 'George'): 1,
+  ('Bob', 'George'): 5,
+  ('George', 'Mallory'): 8,
+  ('Frank', 'George'): 9,
+  ('David', 'Eric'): 33,
+  ('Alice', 'Bob'): 42,
+  ('Carol', 'Mallory'): 43,
+  ('Bob', 'Eric'): 87,
+  ('David', 'George'): 94,
+  ('Carol', 'Frank'): 98,
+  ('Eric', 'Frank'): 118,
+  ('Alice', 'Mallory'): 131,
+  ('Bob', 'Frank'): 132,
+  ('Carol', 'Eric'): 195
+}
--- 2015/day14.py
+++ 2015/day14.py
@@ -1,2 +1,27 @@
+from operator import itemgetter
 from pathlib import Path
+from parse import parse
 lines = Path("data/day14.txt").read_text().splitlines()
+R = {}
+for line in lines:
+  if p := parse("{} can fly {:d} km/s for {:d} seconds, but then must rest for {:d} seconds.",line):
+    w, s, d, r = p.fixed # who, speed, duration, rest
+    R[w] = (s,d,r)
+def distance(r, T=2503):
+  s,d,r = R[r]
+  m,n = divmod(T,d+r)
+  return s*d*m + s*min(d,n)
+def points(T=2503):
+  P = {r:0 for r in R}
+  for s in range(1,T+1):
+    D = dict(sorted(zip(iter(R),map(lambda r: distance(r, s), R.keys())), key=itemgetter(1), reverse=True))
+    m = max(D.values())
+    for r,d in D.items():
+      if d==m:
+        P[r] += 1
+      else:
+        break
+  return max(P.values())
+r = max(iter(R), key=distance)
+p = points()
+print(distance(r), p)
