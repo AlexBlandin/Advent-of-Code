382d975640ee8dc442a059202391012ab22f0343
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-27 22:58:10 +0000

    2015 Day 16

--- 2015/day16.py
+++ 2015/day16.py
@@ -1,9 +1,11 @@
-from pathlib import Path
-lines = Path("data/day16.txt").read_text().splitlines()
-from parse import parse
 from collections import defaultdict
+from pathlib import Path
+from parse import parse
+
+lines = Path("data/day16.txt").read_text().splitlines()
 I = defaultdict(lambda: defaultdict(set))
 H = {}
+
 for line in lines:
   if p := parse("Sue {:d}: {}: {:d}, {}: {:d}, {}: {:d}", line):
     i, t0, v0, t1, v1, t2, v2 = p.fixed
@@ -24,14 +26,13 @@ def retro_encabulator(i):
     v = mfcsam[t]
     if t in H[i]:
       if t in nuclear_decay:
-        if H[i][t]>v: m += 1
+        if H[i][t] > v: m += 1
       if t in modial_interaction:
-        if H[i][t]<v: m += 1
+        if H[i][t] < v: m += 1
   return m
 
 S = {i:sum(1 for t,v in mfcsam.items() if i in I[t][v]) for i in set(range(1, 501))}
 U = {i:retro_encabulator(i) for i in set(range(1, 501))}
 g = max(S, key=S.get)
-print("May the real Auntie Sue please stand up?")
 r = max(U, key=U.get)
 print(g, r)
