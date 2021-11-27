400fe09485ebc1d713404f990923826aee3de7b0
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-11-27 22:56:53 +0000

    2015 Day 16

--- 2015/day16.py
+++ 2015/day16.py
@@ -22,10 +22,11 @@ def retro_encabulator(i):
   m = sum(1 for t,v in mfcsam.items() if i in I[t][v] and t not in outdated)
   for t in outdated:
     v = mfcsam[t]
-    if t in nuclear_decay and t in H[i]:
-      if H[i][t]>v: m += 1
-    if t in modial_interaction and t in H[i]:
-      if H[i][t]<v: m += 1
+    if t in H[i]:
+      if t in nuclear_decay:
+        if H[i][t]>v: m += 1
+      if t in modial_interaction:
+        if H[i][t]<v: m += 1
   return m
 
 S = {i:sum(1 for t,v in mfcsam.items() if i in I[t][v]) for i in set(range(1, 501))}
