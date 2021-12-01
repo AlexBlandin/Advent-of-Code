88714af3f9bf0c13bb13f9c6d02dfcbeaf200082
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-01 23:28:18 +0000

    2021 Day 1

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from parse import parse
 lines = Path("data/day1.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
+lines = [int(line) for line in lines]
+from operator import gt
+from functools import reduce
+print(sum(map(gt,lines[1:],lines)), sum(map(gt,map(sum, zip(lines[1::],lines[2::],lines[3::])),map(sum, zip(lines,lines[1::],lines[2::])))))
