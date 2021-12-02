ee2bd06ac34eafb5ee0a2622c6e5b07dbe2fbecc
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-02 10:53:36 +0000

    2021 Day 2

--- 2021/day2.py
+++ 2021/day2.py
@@ -1,6 +1,19 @@
+from operator import itemgetter
 from pathlib import Path
-from parse import parse
 lines = Path("data/day2.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
+h, v = 0, 0
+a, va = 0, 0
+lines = [((l := line.split())[0], int(l[1])) for line in lines]
+for d, x in lines:
+  match d:
+    case "forward":
+      h += x
+      va += x*a
+    case "down":
+      v += x
+      a += x
+    case "up":
+      v -= x
+      a -= x
+      if v < 0: v = 0
+print(h*v, h*va)
