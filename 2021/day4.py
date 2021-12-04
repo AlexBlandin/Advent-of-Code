86620ad0c276830dec3d2a6ab59015760ebd5f91
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-04 17:17:16 +0000

    2021 Day 4 Cleanup

--- 2021/day4.py
+++ 2021/day4.py
@@ -7,14 +7,13 @@ boardsets = [{a for b in board for a in b} for board in boards]
 for i, bs in enumerate(boardsets):
   for b in bs:
     if b in order: order[b].add(i)
-scores, seen, won = [], set(), set()
+score, seen = {}, set()
 for k, bk in order.items():
   seen.add(k)
   for i in bk:
     boardsets[i].remove(k)
-    five_in_a_row = lambda row: len(set(row) & seen) == 5
-    if i not in won and (any(filter(five_in_a_row, boards[i])) or any(filter(five_in_a_row, zip(*boards[i])))):
-      scores.append(k * sum(boardsets[i]))
-      won.add(i)
-
+    fiveinarow = lambda row: len(set(row) & seen) == 5
+    if i not in score and (any(filter(fiveinarow, boards[i])) or any(filter(fiveinarow, zip(*boards[i])))):
+      score[i] = k * sum(boardsets[i])
+scores = list(score.values())
 print(scores[0], scores[-1])
