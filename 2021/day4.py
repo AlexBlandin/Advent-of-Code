9ee987b5cd2c3737359c2eaa97eec750bba9c496
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-04 17:09:21 +0000

    2021 Day 4

--- 2021/day4.py
+++ 2021/day4.py
@@ -1,6 +1,20 @@
 from pathlib import Path
-from parse import parse
 lines = Path("data/day4.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
+order = {int(k):set() for k in lines[0].split(",")}
+lines = lines[2:]
+boards = [[list(map(int, r.strip().split())) for r in b] for b in zip(lines[0::6],lines[1::6],lines[2::6],lines[3::6],lines[4::6])]
+boardsets = [{a for b in board for a in b} for board in boards]
+for i, bs in enumerate(boardsets):
+  for b in bs:
+    if b in order: order[b].add(i)
+scores, seen, won = [], set(), set()
+for k, bk in order.items():
+  seen.add(k)
+  for i in bk:
+    boardsets[i].remove(k)
+    five_in_a = lambda row: len(set(row)&seen)==5
+    if i not in won and (any(filter(five_in_a, boards[i])) or any(filter(five_in_a, zip(*boards[i])))):
+      scores.append(k * sum(boardsets[i]))
+      won.add(i)
+
+print(scores[0], scores[-1])
