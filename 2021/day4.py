4216b42f80357648cf2ef4e7a733256bf4537d65
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-04 17:18:02 +0000

    2021 Day 4 Cleanup

--- 2021/day4.py
+++ 2021/day4.py
@@ -13,6 +13,6 @@ for k, bk in order.items():
   for i in bk:
     fiveinarow = lambda row: len(set(row) & seen) == 5
     if i not in score and (any(filter(fiveinarow, boards[i])) or any(filter(fiveinarow, zip(*boards[i])))):
-      score[i] = k * sum(boardsets[i]-seen)
+      score[i] = k * sum(boardsets[i] - seen)
 scores = list(score.values())
 print(scores[0], scores[-1])
