84c54302f53d76e8e6e736caa8096836802018a4
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-07 16:23:36 +0000

    2021 Day 6 Cleanup

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,16 +1,13 @@
-from collections import Counter
+from collections import deque
 from functools import reduce
 from pathlib import Path
 ages = list(map(int,Path("data/day6.txt").read_text().split(",")))
-sim = dict(Counter(ages).items())
-def dec(sim,_):
-  d = {k-1:v for k,v in sim.items()}
-  d[8] = d.get(-1,0)
-  d[6] = d.get(-1,0) + d.get(6, 0)
-  if -1 in d: del d[-1]
-  return d
-sim = reduce(dec, range(80), sim)
-after80days = sum(sim.values())
-sim = reduce(dec, range(256-80), sim)
-after256days = sum(sim.values())
+sim = deque(ages.count(i) for i in range(9))
+def step(sim: deque):
+  sim.append(sim.popleft())
+  sim[-3] += sim[-1]
+for _ in range(80): step(sim)
+after80days = sum(sim)
+for _ in range(256-80): step(sim)
+after256days = sum(sim)
 print(after80days, after256days)
