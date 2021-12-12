8191e282996d6859b5078845b6a3dd3a1b67a0a3
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-12 19:56:02 +0000

    2021 Day 10

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,6 +1,27 @@
 from pathlib import Path
-from parse import parse
 lines = Path("data/day10.txt").read_text().splitlines()
+corruption_score, discarded_score = 0, []
 for line in lines:
-  if p := parse("", line):
-    p.fixed
+  s = []
+  n = {"(":0,"[":0,"{":0,"<":0}
+  for c in line:
+    match c:
+      case "(" | "[" | "{" | "<":
+        s.append(c)
+        n[c] += 1
+      case ")" | "]" | "}" | ">":
+        if s[-1]+c in {"()","[]","{}","<>"}:
+          n[s.pop()] -= 1
+        else:
+          corruption_score += {")":3,"]":57,"}":1197,">":25137}[c]
+          break
+  else: # incomplete
+    score = 0
+    for c in s[::-1]:
+      # pair = {"(":")","[":"]","{":"}","<":">"}[c]
+      score *= 5
+      score += {"(":1,"[":2,"{":3,"<":4}[c]
+    discarded_score.append(score)
+
+discarded_score = sorted(discarded_score)[len(discarded_score)//2]
+print(corruption_score, discarded_score)
