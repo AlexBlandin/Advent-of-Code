40c302d2267d10b784a385717651b11c65fc69fd
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2021-12-16 16:11:21 +0000

    2021 Day 11

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,6 +1,28 @@
+from itertools import count
 from pathlib import Path
-from parse import parse
-lines = Path("data/day11.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
+grid = [list(map(int, line)) for line in Path("data/day11.txt").read_text().splitlines()]
+
+def neighbours(x,y): return [(x+xo,y+yo) for xo in (1,-1,0) for yo in (1,-1,0) if (xo,yo)!=(0,0) and x+xo in range(10) and y+yo in range(10)]
+sfilter = lambda f, starlist: filter(lambda s: f(*s), starlist)
+turns, flashes, syncs = 100, 0, 0
+coords = lambda: [(x,y) for x in range(10) for y in range(10)]
+for i in count(1):
+  flashed = [[False for _ in range(10)] for _ in range(10)]
+  for x,y in coords():
+    grid[x][y] += 1
+  while f := list(sfilter(lambda x, y: grid[x][y]>9 and not flashed[x][y], coords())):
+    for x,y in f:
+      flashed[x][y] = True
+      for nx,ny in neighbours(x,y):
+        if not flashed[nx][ny]:
+          grid[nx][ny] += 1
+  have_flashed = sum(map(sum, flashed))
+  if i <= 100: flashes += have_flashed
+  if have_flashed == 100:
+    syncs = i
+    break
+  for x,y in coords():
+    if flashed[x][y]:
+      grid[x][y] = 0
+
+print(flashes, syncs)
