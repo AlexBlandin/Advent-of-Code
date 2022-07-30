4c298e6764799785ee93e1d49993340bb28c2bd1
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-07-30 11:39:06 +0100

    Future proofed

--- prep_files.py
+++ prep_files.py
@@ -1,18 +1,19 @@
 from pathlib import Path
-fmt="""from pathlib import Path
+from datetime import date
+
+fmt = """from pathlib import Path
 from parse import parse
 lines = Path("data/day{day}.txt").read_text().splitlines()
 for line in lines:
   if p := parse("", line):
     p.fixed
 """
-for year in [2015, 2016, 2017, 2018, 2019, 2020, 2021]:
+
+for year in range(2015, date.today().year + 1):
   for day in range(1, 26):
     py = Path(f"{year}/day{day}.py")
     data = Path(f"{year}/data/day{day}.txt")
-    if not py.exists():
-      py.touch()
-      py.write_text(fmt.format(day=day),newline="")
-    if not data.exists():
-      data.touch()
-      data.write_text("",newline="\n")
+    py.touch()
+    data.touch()
+    if not py.exists(): py.write_text(fmt.format(day = day), encoding="utf8", newline = "")
+    if not data.exists(): data.write_text("", encoding="utf8", newline = "\n")
