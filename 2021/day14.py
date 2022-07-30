e6aeefb1258afdb6ac8276f03cf4e12c2f6d9328
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-07-30 18:16:29 +0100

    Day 14, superfast

--- 2021/day14.py
+++ 2021/day14.py
@@ -1,7 +1,28 @@
+from collections import Counter
 from pathlib import Path
-from parse import parse
+
+from tqdm import tqdm
 
 lines = Path("data/day14.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
+base = lines[0]
+insert, letters, pairs = {}, Counter(base), Counter(zip(base, base[1:]))
+for rule in lines[2:]:
+  pair, mid = rule.split(" -> ", maxsplit = 1)
+  insert[tuple(pair)] = mid
+
+def ex(insert: dict, letters: Counter, pairs: Counter):
+  for p, n in list(pairs.items()):
+    p0, p1 = p
+    l = insert[p]
+    pairs[p] -= n
+    letters[l] += n
+    pairs[(p0, l)] += n
+    pairs[(l, p1)] += n
+
+for i in tqdm(range(40)):
+  ex(insert, letters, pairs)
+  if i == 9:
+    c = max(letters.values()) - min(letters.values())
+d = max(letters.values()) - min(letters.values())
+
+print(c, d)
