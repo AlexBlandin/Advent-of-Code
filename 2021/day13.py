b94241d09f75884651261e431f3e12b64487aa0c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-07-30 13:10:33 +0100

    Day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,4 +1,4 @@
-from operator import itemgetter
+from operator import itemgetter, or_
 from pathlib import Path
 from parse import parse
 
@@ -9,4 +9,37 @@ for line in lines:
     dots.append(p.fixed)
   elif p := parse("fold along {}={:d}", line):
     folds.append(p.fixed)
-w, h = max(map(itemgetter(0), dots)), max(map(itemgetter(1), dots))
+
+w, h = max(map(itemgetter(0), dots)) + 1, max(map(itemgetter(1), dots)) + 1
+paper = [[False] * w for _ in range(h)]
+for x, y in dots:
+  paper[y][x] = True
+
+def fold_over(line: int):
+  for row in paper:
+    a, b = row[:line], row[line + 1:]
+    if len(a) < len(b):
+      a += [False] * (len(a) - len(b))
+    else:
+      b += [False] * (len(b) - len(a))
+    row[:] = list(map(or_, a, reversed(b)))
+
+def fold_up(line: int):
+  a, b = paper[:line], paper[line + 1:]
+  if len(a) < len(b):
+    a += [[False] * w for _ in range(len(a) - len(b))]
+  else:
+    b += [[False] * w for _ in range(len(b) - len(a))]
+  paper[:] = [list(map(or_, ra, rb)) for ra,rb in zip(a, reversed(b))]
+
+first_fold, first_line = folds.pop(0)
+if first_fold == "x": fold_over(first_line)
+else: fold_up(first_line)
+print(sum(map(sum, paper)))
+
+for fold, line in folds:
+  if fold == "x": fold_over(line)
+  else: fold_up(line)
+
+for row in paper:
+  print("".join(map(lambda b: "#" if b else ".", row)))
