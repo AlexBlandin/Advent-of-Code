4906b22e55485523f89847724ef9a2a879f80da0
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-08-03 15:42:23 +0100

    Touch is unnecessary when using write_text

--- prep_files.py
+++ prep_files.py
@@ -19,7 +19,5 @@ for year in range(2015, date.today().year + 1):
   for day in range(1, 26):
     py = dir / f"day{day}.py"
     data = dta / f"day{day}.txt"
-    py.touch()
-    data.touch()
     if py.read_text().strip() == "": py.write_text(fmt.format(day = day), encoding = "utf8", newline = "")
     if data.read_text().strip() == "": data.write_text("", encoding = "utf8", newline = "\n")
