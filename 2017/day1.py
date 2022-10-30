fb3c3e9ec5434fbfd2be631e1873fd30574bfca3
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-10-30 21:15:57 +0000

    Days 1-3

--- 2017/day1.py
+++ 2017/day1.py
@@ -1,2 +1,8 @@
 from pathlib import Path
-lines = Path("day1.txt").read_text().splitlines()
+
+numbers = list(map(int, Path("day1.txt").read_text().strip()))
+
+circular = numbers + [numbers[0]]
+twocular = numbers + numbers[:len(numbers)//2]
+
+print(sum([a for a,b in zip(circular, circular[1:]) if a==b]), sum([a for a,b in zip(twocular, twocular[len(numbers)//2:]) if a==b]))
--- 2017/day3.py
+++ 2017/day3.py
@@ -1,2 +1,8 @@
+from itertools import takewhile, count
 from pathlib import Path
-lines = Path("day3.txt").read_text().splitlines()
+from math import ceil, sqrt
+
+position = int(Path("day3.txt").read_text().strip()) # hi Ulam
+spokes = list(takewhile(lambda n: n <= position, (ceil((n**2 + n + 1) / 4) for n in count())))
+spokes += [ceil(((len(spokes))**2 + (len(spokes)) + 1) / 4)]
+print(ceil((sqrt(position)-1)/2) + min(abs(position - spokes[-1]), abs(position - spokes[-2])), )
