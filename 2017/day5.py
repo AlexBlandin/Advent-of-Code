ee06ab3b3a1d3fde36f89daabbbcd5216dbd7dc7
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-01 20:25:24 +0000

    Day 5

--- 2017/day5.py
+++ 2017/day5.py
@@ -1,3 +1,19 @@
 from pathlib import Path
 
-jumps = list(map(int, Path("day5.txt").read_text().splitlines()))
+jump = list(map(int, Path("day5.txt").read_text().splitlines()))
+i, step = 0, 0
+while 0 <= i < len(jump):
+  j = jump[i]
+  jump[i] += 1
+  i += j
+  step += 1
+
+jump = list(map(int, Path("day5.txt").read_text().splitlines()))
+i, stumble = 0, 0
+while 0 <= i < len(jump):
+  j = jump[i]
+  jump[i] += 1 if j < 3 else -1
+  i += j
+  stumble += 1
+
+print(step, stumble)
