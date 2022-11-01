ed8312bee50922ca606783379377fe0c5c1ddf0e
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-01 02:08:30 +0000

    Day 4

--- 2017/day4.py
+++ 2017/day4.py
@@ -1,2 +1,5 @@
+from collections import Counter
 from pathlib import Path
-lines = Path("day4.txt").read_text().splitlines()
+
+pp = list(map(str.split, Path("day4.txt").read_text().splitlines()))
+print(sum(all(c==1 for c in Counter(p).values()) for p in pp), sum(all(c==1 for c in Counter(map(frozenset, p)).values()) for p in pp))
