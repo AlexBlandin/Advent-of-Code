3b39a69e76bfaa5f8c5961ca653269f48724b590
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-02 22:12:23 +0000

    Part 2 separated

--- 2017/day3.py
+++ 2017/day3.py
@@ -5,10 +5,10 @@ from math import sqrt, ceil
 position = int(Path("day3.txt").read_text().strip()) # hi Ulam
 spokes = list(takewhile(lambda n: n <= position, (ceil((n**2 + n + 1) / 4) for n in count())))
 spokes += [ceil(((len(spokes))**2 + (len(spokes)) + 1) / 4)]
+
 width = ceil((sqrt(position) - 1) / 2) * 2
 x, y = width // 2, width // 2
 m = [[0] * width for _ in range(width)]
-
 neighbours = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
 m[y][x] = acc = 1
 x += 1
