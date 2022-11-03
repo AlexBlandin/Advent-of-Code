8af3f9fc4f99f8b2d31ed4e02684596cab18a2d3
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-03 22:59:22 +0000

    You must all suffer as I did

--- 2017/day6.py
+++ 2017/day6.py
@@ -9,8 +9,7 @@ while tuple(banks) not in history:
   history[tuple(banks)] = len(history)
   i, mx = max(enumerate(banks), key = itemgetter(1))
   div, mod = divmod(mx, lb)
-  rem = [0 if (i - (lb - mod)) < j <= i or (i - (lb - mod) + 1 < 0 and lb - ((lb - mod) - i) < j < lb) else 1 for j in range(lb)]
-  banks[:] = map(add, map(add, banks, [div] * lb), rem)
+  banks[:] = map(add, map(add, banks, [div] * lb), [0 if (i - (lb - mod)) < j <= i or (i - (lb - mod) + 1 < 0 and lb - ((lb - mod) - i) < j < lb) else 1 for j in range(lb)])
   banks[i] = 0
 
 print(len(history), len(history) - history[tuple(banks)])
