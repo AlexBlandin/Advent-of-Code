c15fc7499b29d1bdc00c649a1cf5fa06f8140c3c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-03 23:01:51 +0000

    Enjoy

--- 2017/day6.py
+++ 2017/day6.py
@@ -2,14 +2,13 @@ from operator import add, itemgetter
 from pathlib import Path
 
 banks = list(map(int, Path("day6.txt").read_text().split()))
-lb = len(banks)
-history = {}
+n, history = len(banks), {}
 
 while tuple(banks) not in history:
   history[tuple(banks)] = len(history)
   i, mx = max(enumerate(banks), key = itemgetter(1))
-  div, mod = divmod(mx, lb)
-  banks[:] = map(add, map(add, banks, [div] * lb), [0 if (i - (lb - mod)) < j <= i or (i - (lb - mod) + 1 < 0 and lb - ((lb - mod) - i) < j < lb) else 1 for j in range(lb)])
+  div, rem = divmod(mx, n)
+  banks[:] = map(add, map(add, banks, [div] * n), [0 if (i - (n - rem)) < j <= i or (i - (n - rem) + 1 < 0 and n - ((n - rem) - i) < j < n) else 1 for j in range(n)])
   banks[i] = 0
 
 print(len(history), len(history) - history[tuple(banks)])
