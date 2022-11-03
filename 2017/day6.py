db21b380c0d96100d73627f3e1009ccad9795504
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-03 22:55:30 +0000

    Not happy with how long that took but it's done

--- 2017/day6.py
+++ 2017/day6.py
@@ -1,3 +1,19 @@
+from operator import add, itemgetter
 from pathlib import Path
 
-lines = Path("day6.txt").read_text().splitlines()
+banks = list(map(int, Path("day6.txt").read_text().split()))
+lb, sb = len(banks), sum(banks)
+history = {}
+
+while tuple(banks) not in history:
+  history[tuple(banks)] = len(history)
+  assert sum(banks) == sb
+  assert len(banks) == lb
+  i, mx = max(enumerate(banks), key = itemgetter(1))
+  banks[i] = 0
+  div, mod = divmod(mx, lb)
+  zeroes = lb - mod
+  rem = [0 if (i - zeroes) < j <= i or (i - zeroes + 1 < 0 and lb - (zeroes - i) < j < lb) else 1 for j in range(lb)]
+  banks[:] = map(add, map(add, banks, [div] * lb), rem)
+
+print(len(history), len(history) - history[tuple(banks)])
