d1eae0fb74e89e87ea45da49f6654a305339d6ae
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-05 16:49:13 +0000

    Day 9

--- 2017/day9.py
+++ 2017/day9.py
@@ -1,3 +1,28 @@
 from pathlib import Path
 
-lines = Path("day9.txt").read_text().splitlines()
+stream = Path("day9.txt").read_text().strip()
+cancel, garbage = False, False
+depth, score, removed = 0, 0, 0
+
+for i, c in enumerate(stream):
+  if cancel:
+    cancel = False
+    continue
+  match c:
+    case "!" if garbage:
+      cancel = True
+    case ">" if garbage:
+      garbage = False
+    case "<" if not garbage:
+      garbage = True
+    case "{" if not garbage:
+      depth += 1
+      score += depth
+    case "}" if not garbage:
+      depth -= 1
+    case "," if not garbage:
+      pass
+    case _ if garbage:
+      removed += 1
+
+print(score, removed)
