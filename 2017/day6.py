2ed3cb0b9a2caab7b64edd3a0eede0a4dfc036f1
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-06 18:03:05 +0000

    Circular approach for Day 6 and cleanup

--- 2017/day6.py
+++ 2017/day6.py
@@ -1,4 +1,4 @@
-from operator import add, itemgetter
+from operator import itemgetter, add
 from pathlib import Path
 
 banks = list(map(int, Path("day6.txt").read_text().split()))
@@ -7,8 +7,11 @@ n, history = len(banks), {}
 while tuple(banks) not in history:
   history[tuple(banks)] = len(history)
   i, mx = max(enumerate(banks), key = itemgetter(1))
-  div, rem = divmod(mx, n)
-  banks[:] = map(add, map(add, banks, [div] * n), [0 if (i - (n - rem)) < j <= i or (i - (n - rem) + 1 < 0 and n - ((n - rem) - i) < j < n) else 1 for j in range(n)])
   banks[i] = 0
+  for i in range(i + 1, i + 1 + mx):
+    banks[i % len(banks)] += 1 # mx ended up being small, so this is fine
+  if False:
+    div, rem = divmod(mx, n)
+    banks[:] = map(add, map(add, banks, [div] * n), [0 if (i - (n - rem)) < j <= i or (i - (n - rem) + 1 < 0 and n - ((n - rem) - i) < j < n) else 1 for j in range(n)])
 
 print(len(history), len(history) - history[tuple(banks)])
--- 2017/day6circular.py
+++ 2017/day6circular.py
@@ -1,4 +1,5 @@
 from itertools import count
+from operator import itemgetter, add
 from pathlib import Path
 from typing import SupportsIndex
 
@@ -25,4 +26,13 @@ class Circular(list):
       for _ in range(times):
         yield from iter(self)
 
-lst = Path("day10.txt").read_text().strip().split(",")
+banks = Circular(map(int, Path("day6.txt").read_text().split()))
+n, history = len(banks), {}
+
+while tuple(banks) not in history:
+  history[tuple(banks)] = len(history)
+  i, mx = max(enumerate(banks), key = itemgetter(1))
+  banks[i] = 0
+  banks[:] = map(add, banks.repeat(), [0] * (i+1) + [1] * mx)
+
+print(len(history), len(history) - history[tuple(banks)])
