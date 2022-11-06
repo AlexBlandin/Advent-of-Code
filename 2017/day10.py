b9bedf3773c4753a9783ad302e1b1c7a1359960c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-06 18:06:20 +0000

    Refactored

--- 2017/day10.py
+++ 2017/day10.py
@@ -2,27 +2,6 @@ from itertools import count
 from pathlib import Path
 from typing import SupportsIndex
 
-class Circular(list):
-  """a circularly addressable list, where Circular([0, 1, 2, 3, 4])[-5:10] is [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]"""
-  def __getitem__(self, x: int | slice):
-    if isinstance(x, slice):
-      return [self[x] for x in range(0 if x.start is None else x.start, len(self) if x.stop is None else x.stop, 1 if x.step is None else x.step)]
-    return super().__getitem__(x.__index__() % max(1, len(self)))
-  
-  def __setitem__(self, x: SupportsIndex | slice, val):
-    if isinstance(x, slice) and (hasattr(val, "__iter__") or hasattr(val, "__getitem__")):
-      m = max(1, len(self))
-      for i, v in zip(count(0 if x.start is None else x.start, 1 if x.step is None else x.step), val) if x.stop is None else zip(range(0 if x.start is None else x.start, len(self) if x.stop is None else x.stop, 1 if x.step is None else x.step), val):
-        super().__setitem__(i.__index__() % m, v)
-    else:
-      super().__setitem__(x.__index__() % max(1, len(self)), val)
-
-  def repeat(self, times: None | int = None):
-    if times is None:
-      while True:
-        yield from iter(self)
-    else:
-      for _ in range(times):
-        yield from iter(self)
+from circular import Circular
 
 lst = Path("day10.txt").read_text().strip().split(",")
