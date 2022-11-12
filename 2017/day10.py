01092d981b12f07226743f3bedb7342d793040ae
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-12 17:22:19 +0000

    Cleanup

--- 2017/day10.py
+++ 2017/day10.py
@@ -1,28 +1,15 @@
 from functools import reduce
 from operator import xor
 from pathlib import Path
+from math import prod
 
 from circular import Circular
 
-lengths = list(map(int, Path("day10.txt").read_text().strip().split(",")))
-circle = Circular(range(256))
-
-pos = skip = 0
-for l in lengths:
-  rev = list(reversed(circle[pos:pos + l]))
-  circle[pos:pos + l] = circle[pos + l - 1:pos - 1:-1]
-  assert circle[pos:pos + l] == rev
-  pos += skip + l
-  skip += 1
-
-part1 = circle[0] * circle[1]
 lengths = list(Path("day10.txt").read_bytes().strip()) + [17, 31, 73, 47, 23]
-circle = Circular(range(256))
-pos = skip = 0
-
 # lengths = list(b"AoC 2017") + [17, 31, 73, 47, 23]
+circle, pos, skip = Circular(range(256)), 0, 0
 
-def round(circle, lengths, pos, skip):
+def knot(lengths, circle = Circular(range(256)), pos = 0, skip = 0):
   for l in lengths:
     rev = list(reversed(circle[pos:pos + l]))
     circle[pos:pos + l] = circle[pos + l - 1:pos - 1:-1]
@@ -32,9 +19,6 @@ def round(circle, lengths, pos, skip):
   return circle, pos, skip
 
 for _ in range(64):
-  circle, pos, skip = round(circle, lengths, pos, skip)
-print(circle)
+  circle, pos, skip = knot(lengths, circle, pos, skip)
 
-dense = bytes([reduce(xor, circle[i * 16:(i + 1) * 16]) for i in range(16)]).hex()
-
-print(part1, dense)
+print(prod(knot(list(map(int, Path("day10.txt").read_text().strip().split(","))))[0][:2]), bytes(reduce(xor, circle[i * 16:(i + 1) * 16]) for i in range(16)).hex())
--- 2017/day11.py
+++ 2017/day11.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-lines = Path("day11.txt").read_text().splitlines()
+lines = Path("day11.txt").read_text().split()
