9af725bb24a761ad4776bab1ed63c33e90dab5f6
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-12 22:20:50 +0000

    Day 11

--- 2017/day11.py
+++ 2017/day11.py
@@ -1,3 +1,25 @@
 from pathlib import Path
 
-lines = Path("day11.txt").read_text().split()
+from hexagon import Hex
+
+dirs = Path("day11.txt").read_text().split(",")
+pos = origin = Hex(0, 0, 0)
+distance = furthest = 0
+for d in dirs:
+  match d:
+    case "n":
+      pos = pos.nn
+    case "s":
+      pos = pos.ss
+    case "nw":
+      pos = pos.nw
+    case "ne":
+      pos = pos.ne
+    case "sw":
+      pos = pos.sw
+    case "se":
+      pos = pos.se
+  distance = pos.distance(origin)
+  furthest = max(furthest, distance)
+
+print(distance - 1, furthest)
