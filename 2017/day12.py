934148b4ffc73b1e97c54ecf97d698e6b131b7ac
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 04:25:50 +0000

    Major speedup

--- 2017/day12.py
+++ 2017/day12.py
@@ -11,9 +11,9 @@ for p in lines:
         pipes[b].update(p)
 
 for p in pipes: # the pipes[b].update loop handles feed-forward/backward, so only 1 pass is needed
-  pipes[p].update(*[pipes[b] for b in pipes[p]]) # set.update accepts multiple sets to union over!
-  for b in pipes[p]:
+  for b in pipes[p]: # do this first, because that makes these updates way smaller, so way faster!
     pipes[b].update(pipes[p])
-groups = [inverse.update(bs) for p, bs in pipes.items() if p not in inverse]
+  pipes[p].update(*[pipes[b] for b in pipes[p]]) # set.update accepts multiple sets to union over!
 
+groups = [inverse.update(bs) for p, bs in pipes.items() if p not in inverse]
 print(len(pipes[0]), len(groups))
