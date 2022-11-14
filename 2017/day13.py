c82b414f55b5ff2755cfeff222ce1041a6c53b94
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 04:14:45 +0000

    Major speedup

--- 2017/day13.py
+++ 2017/day13.py
@@ -4,5 +4,5 @@ from pathlib import Path
 lines = dict(tuple(map(int, line.split())) for line in Path("day13.txt").read_text().replace(":", "").splitlines())
 print(
   sum(d * r for d, r in lines.items() if d % (2 * r - 2) == 0),
-  next(i for i in count() if sum(1 for d, r in lines.items() if (d + i) % (2 * r - 2) == 0) == 0)
+  next(i for i in count() if not any(1 for d, r in lines.items() if (d + i) % (2 * r - 2) == 0))
 )
