da64273984792deec506aa76ccbcd7e554b9e687
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 04:12:35 +0000

    Day 13

--- 2017/day13.py
+++ 2017/day13.py
@@ -1,3 +1,8 @@
+from itertools import count
 from pathlib import Path
 
-lines = dict(tuple(map(int, line.split())) for line in Path("day13.txt").read_text().replace(":","").splitlines())
+lines = dict(tuple(map(int, line.split())) for line in Path("day13.txt").read_text().replace(":", "").splitlines())
+print(
+  sum(d * r for d, r in lines.items() if d % (2 * r - 2) == 0),
+  next(i for i in count() if sum(1 for d, r in lines.items() if (d + i) % (2 * r - 2) == 0) == 0)
+)
