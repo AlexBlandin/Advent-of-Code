787bba0ce49af8f7d22c621c610b0773977db16d
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 05:08:48 +0000

    Speedup

--- 2017/day12.py
+++ 2017/day12.py
@@ -3,17 +3,17 @@ from pathlib import Path
 
 lines = Path("day12.txt").read_text().replace(",", "").replace("<->","").splitlines()
 pipes, inverse = defaultdict(set), set()
-for p in lines:
-  match list(map(int, p.split())):
-    case [a, *bs] as p:
-      pipes[a].update(p)
+for ps in lines:
+  match list(map(int, ps.split())):
+    case [p, *bs] as ps:
+      pipes[p].update(ps)
       for b in bs:
-        pipes[b].update(p)
+        pipes[b].update(ps)
 
-for p in pipes: # the pipes[b].update loop handles feed-forward/backward, so only 1 pass is needed
-  for b in pipes[p]: # do this first, because that makes these updates way smaller, so way faster!
-    pipes[b].update(pipes[p])
-  pipes[p].update(*[pipes[b] for b in pipes[p]]) # set.update accepts multiple sets to union over!
+for p, bs in pipes.items(): # bs.update loop handles feed-forward/backward, one pass needed!
+  for b in bs: # do this first, because that makes these updates way smaller, so way faster!
+    pipes[b].update(bs)
+  bs.update(*[pipes[b] for b in pipes[p]]) # set.update accepts multiple sets to union over!
 
 groups = [inverse.update(bs) for p, bs in pipes.items() if p not in inverse]
 print(len(pipes[0]), len(groups))
--- 2017/day14.py
+++ 2017/day14.py
@@ -1,3 +1,5 @@
 from pathlib import Path
 
+from circular import Circular
+
 lines = Path("day14.txt").read_text().splitlines()
