c6d1a5fce96766b7d2310bc25cc032c6141bfa90
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 05:28:11 +0000

    might as well be consistent

--- 2017/day12.py
+++ 2017/day12.py
@@ -13,7 +13,7 @@ for ps in lines:
 # for p, bs in pipes.items(): # bs.update loop handles feed-forward/backward, one pass needed!
 #   for b in bs: # do this first, because that makes these updates way smaller, so way faster!
 #     pipes[b].update(bs)
-#   bs.update(*[pipes[b] for b in bs]) # set.update accepts multiple sets to union over!
+#   bs.update(*[pipes[b] for b in bs]) # bs.update accepts comma-separated sets to union over!
 for p, bs in pipes.items():
   bs.update(*[pipes[b].update(bs) or pipes[b] for b in bs])
 
