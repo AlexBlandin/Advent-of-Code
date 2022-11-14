e3aba7e95e1492ef5bcc1c32c3262b280e5778f9
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-11-14 00:21:56 +0000

    Simplify

--- 2017/day12.py
+++ 2017/day12.py
@@ -1,32 +1,25 @@
 from collections import defaultdict
 from pathlib import Path
 
-lines = Path("day12.txt").read_text().splitlines()
+lines = Path("day12.txt").read_text().replace(",", "").splitlines()
 pipes: defaultdict[int, set[int]] = defaultdict(set)
 for p in lines:
   match p.split():
     case [a, "<->", *bs]:
-      a = int(a)
-      bs = list(map(int, "".join(bs).split(",")))
-      bs.append(a)
-      pipes[a].update(bs)
+      bs = list(map(int, bs+[a]))
+      pipes[int(a)].update(bs)
       for b in bs:
         pipes[b].update(bs)
 
-s = -1
-while s != sum(map(len, pipes.values())):
-  s = sum(map(len, pipes.values()))
-  for p in pipes:
-    for b in list(pipes[p]):
-      pipes[p].update(pipes[b])
-      pipes[b].update(pipes[p])
+for p in pipes:
+  for b in list(pipes[p]):
+    pipes[p].update(pipes[b])
+    pipes[b].update(pipes[p])
 
-inverse = {}
-groups = 0
+inverse, groups = {}, 0
 for p in pipes:
   if p not in inverse:
     groups += 1
-    inverse[p] = p
     for b in pipes[p]:
       inverse[b] = p
 
--- 2017/day13.py
+++ 2017/day13.py
@@ -1,3 +1,9 @@
 from pathlib import Path
 
-lines = Path("day13.txt").read_text().splitlines()
+lines = Path("day13.txt").read_text().replace(":","").splitlines()
+for i, line in enumerate(lines):
+  match line.split():
+    case [a, b]:
+      pass
+    case _:
+      raise SyntaxError(f"parse error on line {i+1}")
