383755707be017d9bb8e118ab5d9c03d183b2089
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-01 18:42:15 +0000

    Start without parse by default

--- 2021/day23.py
+++ 2021/day23.py
@@ -1,7 +1,3 @@
 from pathlib import Path
-from parse import parse
 
 lines = Path("day23.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
--- 2022/day21.py
+++ 2022/day21.py
@@ -1,8 +1,3 @@
 from pathlib import Path
-from parse import parse
 
 lines = Path("day21.txt").read_text().splitlines()
-for line in lines:
-  if p := parse("", line):
-    p.fixed
-
