8226c387f5a60718e5632201e2bee17783c66027
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-03 19:46:49 +0000

    ascii_letters doesn't follow ascii/ord! nice!

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,5 +1,4 @@
-from itertools import chain
 from pathlib import Path
-from string import ascii_uppercase, ascii_lowercase
+from string import ascii_letters
 
-print(sum(map(lambda rucksack: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]))), rucksacks := Path("day3.txt").read_text().splitlines())), sum(map(lambda a, b, c: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(a) & set(b) & set(c))), rucksacks[::3], rucksacks[1::3], rucksacks[2::3])))
+print(sum(map(lambda rucksack: sum(map(dict(zip(ascii_letters, range(1, 53))).get, set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]))), rucksacks := Path("day3.txt").read_text().splitlines())), sum(map(lambda a, b, c: sum(map(dict(zip(ascii_letters, range(1, 53))).get, set(a) & set(b) & set(c))), rucksacks[::3], rucksacks[1::3], rucksacks[2::3])))
