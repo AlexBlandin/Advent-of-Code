14ff57cfd39f4e80528b329efb0de429135bb490
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-03 18:48:35 +0000

    Don't need starmap now

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,5 +1,5 @@
-from itertools import starmap, chain
+from itertools import chain
 from pathlib import Path
 from string import ascii_uppercase, ascii_lowercase
 
-print(sum(map(lambda rucksack: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]))), rucksacks := Path("day3.txt").read_text().splitlines())), sum(starmap(lambda a, b, c: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(a) & set(b) & set(c))), zip(rucksacks[::3], rucksacks[1::3], rucksacks[2::3]))))
+print(sum(map(lambda rucksack: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]))), rucksacks := Path("day3.txt").read_text().splitlines())), sum(map(lambda a, b, c: sum(map(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53)))).get, set(a) & set(b) & set(c))), rucksacks[::3], rucksacks[1::3], rucksacks[2::3])))
