a93c527d23a725ea91934f726a69d28c997157c4
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-03 15:44:58 +0000

    Redundant nesting gone

--- 2022/day3.py
+++ 2022/day3.py
@@ -2,4 +2,4 @@ from itertools import starmap, chain
 from pathlib import Path
 from string import ascii_uppercase, ascii_lowercase
 
-print(sum(map(lambda items: sum(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53))))[item] for item in items), common := list(map(lambda rucksack: set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]), rucksacks := Path("day3.txt").read_text().splitlines())))), sum(map(lambda items: sum(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53))))[item] for item in items), starmap(lambda a, b, c: set(a) & set(b) & set(c), zip(rucksacks[::3], rucksacks[1::3], rucksacks[2::3])))))
+print(sum(map(lambda rucksack: sum(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53))))[item] for item in (set(rucksack[:len(rucksack)//2]) & set(rucksack[len(rucksack)//2:]))), rucksacks := Path("day3.txt").read_text().splitlines())), sum(starmap(lambda a, b, c: sum(dict(chain(zip(ascii_lowercase, range(1, 27)), zip(ascii_uppercase, range(27, 53))))[item] for item in (set(a) & set(b) & set(c))), zip(rucksacks[::3], rucksacks[1::3], rucksacks[2::3]))))
