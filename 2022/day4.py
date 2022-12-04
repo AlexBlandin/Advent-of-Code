f77ac2f3497dea39e160bfa5ae2f2c04ada0ace4
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-04 16:17:59 +0000

    Back to starmap, life's better this way

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,3 +1,4 @@
+from itertools import starmap
 from pathlib import Path
 
-print(sum(map(lambda job: (job[0] <= job[2] <= job[1] and job[0] <= job[3] <= job[1]) or (job[2] <= job[0] <= job[3] and job[2] <= job[1] <= job[3]), jobs := list(map(lambda line: tuple(map(int, line.split(","))), Path("day4.txt").read_text().replace("-",",").splitlines())))), sum(map(lambda job: job[0] <= job[2] <= job[1] or job[0] <= job[3] <= job[1] or job[2] <= job[0] <= job[3] or job[2] <= job[1] <= job[3], jobs)))
+print(sum(starmap(lambda a, b, c, d: (a <= c <= b and a <= d <= b) or (c <= a <= d and c <= b <= d), jobs := list(map(lambda line: tuple(map(int, line.split(","))), Path("day4.txt").read_text().replace("-",",").splitlines())))), sum(starmap(lambda a, b, c, d: a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d, jobs)))
