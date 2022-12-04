2806e89fbda65db86c060ead199eaf375b0741f2
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-04 14:51:53 +0000

    Day 4

--- 2022/day4.py
+++ 2022/day4.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-lines = Path("day4.txt").read_text().splitlines()
+print(sum(map(lambda job: (job[0][0] <= job[1][0] <= job[0][1] and job[0][0] <= job[1][1] <= job[0][1]) or (job[1][0] <= job[0][0] <= job[1][1] and job[1][0] <= job[0][1] <= job[1][1]), jobs := list(map(lambda line: tuple((map(lambda job: tuple(map(int, job)), line))), list(map(lambda l: list(map(lambda j: j.split("-"), l.split(","))), Path("day4.txt").read_text().splitlines())))))), sum(map(lambda job: job[0][0] <= job[1][0] <= job[0][1] or job[0][0] <= job[1][1] <= job[0][1] or job[1][0] <= job[0][0] <= job[1][1] or job[1][0] <= job[0][1] <= job[1][1], jobs)))
