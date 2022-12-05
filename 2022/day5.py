6d586346044f42e7e67b969a6e9cad36a4b6c895
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-05 13:28:18 +0000

    Not a one-liner... yet

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,3 +1,17 @@
+from operator import itemgetter
 from pathlib import Path
 
-lines = Path("day5.txt").read_text().splitlines()
+stacks, moves = Path("day5.txt").read_text().split("\n\n", maxsplit = 1)
+stacks = list(filter(None, map(list, map(lambda s: filter(str.isalpha, s), map(list, map(reversed, zip(*stacks.splitlines()[:-1])))))))
+stack2 = [s[:] for s in stacks]
+moves = list(map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, moves.splitlines())))
+for n, a, b in moves:
+  a, b = a - 1, b - 1
+  for _ in range(n):
+    stacks[b].append(stacks[a].pop())
+for n, a, b in moves:
+  a, b = a - 1, b - 1
+  stack2[b] += stack2[a][-n:]
+  stack2[a] = stack2[a][:-n]
+print("".join(map(itemgetter(-1), stacks)), "".join(map(itemgetter(-1), stack2)))
+# QMBMJDFTD NBTVTJNFJ
