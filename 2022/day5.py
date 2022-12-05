17c78d723b0e8d7ab9d04aa8743a3094ac8fca15
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-05 14:37:54 +0000

    Cleaner with a starmap

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,17 +1,15 @@
+from itertools import starmap
 from operator import itemgetter
 from pathlib import Path
 
-stacks, moves = Path("day5.txt").read_text().split("\n\n", maxsplit = 1)
-stacks = list(filter(None, map(list, map(lambda s: filter(str.isalpha, s), map(list, map(reversed, zip(*stacks.splitlines()[:-1])))))))
-stack2 = [s[:] for s in stacks]
-moves = list(map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, moves.splitlines())))
+crane1 = list(map(list, stacks := list(filter(None, map(list, map(lambda s: filter(str.isalpha, s), map(list, map(reversed, zip(*(inpt := Path("day5.txt").read_text().split("\n\n", maxsplit = 1))[0].splitlines()[:-1])))))))))
+crane2 = list(map(list, stacks))
+moves = list(starmap(lambda n, a, b: (n, a-1, b-1), map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, inpt[1].splitlines()))))
 for n, a, b in moves:
-  a, b = a - 1, b - 1
   for _ in range(n):
-    stacks[b].append(stacks[a].pop())
+    crane1[b].append(crane1[a].pop())
 for n, a, b in moves:
-  a, b = a - 1, b - 1
-  stack2[b] += stack2[a][-n:]
-  stack2[a] = stack2[a][:-n]
-print("".join(map(itemgetter(-1), stacks)), "".join(map(itemgetter(-1), stack2)))
+  crane2[b] += crane2[a][-n:]
+  crane2[a] = crane2[a][:-n]
+print("".join(map(itemgetter(-1), crane1)), "".join(map(itemgetter(-1), crane2)))
 # QMBMJDFTD NBTVTJNFJ
