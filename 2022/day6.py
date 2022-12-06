3c81eb51af8cc82977cc8af61fffc2d237f8dc8c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-06 16:56:36 +0000

    Day 6

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-lines = Path("day6.txt").read_text().splitlines()
+print(next(filter(None, map(lambda a, b, c, d, i: i if len({a, b, c, d})==4 else None, data := Path("day6.txt").read_text(), data[1:], data[2:], data[3:], range(4, len(data))))), next(filter(None, map(lambda a, b, c, d, e, f, g, h, j, k, l, m, n, o, i: i if len({a, b, c, d, e, f, g, h, j, k, l, m, n, o})==14 else None, data, data[1:], data[2:], data[3:], data[4:], data[5:], data[6:], data[7:], data[8:], data[9:], data[10:], data[11:], data[12:], data[13:], range(14, len(data))))))
