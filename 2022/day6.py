589b87c72a5693a080f96b85954521bfaa31a9fb
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-06 17:02:28 +0000

    Clean

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,3 +1,5 @@
+from itertools import starmap
 from pathlib import Path
 
-print(next(filter(None, map(lambda a, b, c, d, i: i if len({a, b, c, d})==4 else None, data := Path("day6.txt").read_text(), data[1:], data[2:], data[3:], range(4, len(data))))), next(filter(None, map(lambda a, b, c, d, e, f, g, h, j, k, l, m, n, o, i: i if len({a, b, c, d, e, f, g, h, j, k, l, m, n, o})==14 else None, data, data[1:], data[2:], data[3:], data[4:], data[5:], data[6:], data[7:], data[8:], data[9:], data[10:], data[11:], data[12:], data[13:], range(14, len(data))))))
+print(next(filter(None, starmap(lambda i, tup: i if len(set(tup))==4 else None, enumerate(zip(data := Path("day6.txt").read_text(), data[1:], data[2:], data[3:]), 4)))), next(filter(None, starmap(lambda i, tup: i if len(set(tup))==14 else None, enumerate(zip(*[data[o:] for o in range(14)]), 14)))))
+  
