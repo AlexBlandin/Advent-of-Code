fcb0a127b9ea20f710fee3ed346e36b1f340719c
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-06 17:02:38 +0000

    Tab somehow got in

--- 2022/day6.py
+++ 2022/day6.py
@@ -2,4 +2,3 @@ from itertools import starmap
 from pathlib import Path
 
 print(next(filter(None, starmap(lambda i, tup: i if len(set(tup))==4 else None, enumerate(zip(data := Path("day6.txt").read_text(), data[1:], data[2:], data[3:]), 4)))), next(filter(None, starmap(lambda i, tup: i if len(set(tup))==14 else None, enumerate(zip(*[data[o:] for o in range(14)]), 14)))))
-  
