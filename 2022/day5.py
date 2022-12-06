28045dca8965b5aeb6ef52f3d4cff41303f7126d
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-06 00:23:05 +0000

    Spot the redundancy

--- 2022/day5.py
+++ 2022/day5.py
@@ -2,8 +2,9 @@ from itertools import starmap
 from operator import itemgetter
 from pathlib import Path
 
-crane1 = list(map(list, stacks := list(filter(None, map(lambda s: list(filter(str.isalpha, s)), map(list, map(reversed, zip(*(inpt := Path("day5.txt").read_text().split("\n\n", maxsplit = 1))[0].splitlines()[:-1]))))))))
-crane2 = list(map(list, stacks))
+inpt = Path("day5.txt").read_text().split("\n\n", maxsplit = 1)
+crane1 = list(filter(None, map(lambda s: list(filter(str.isalpha, s)), map(list, map(reversed, zip(*inpt[0].splitlines()[:-1]))))))
+crane2 = list(map(list, crane1))
 moves = list(starmap(lambda n, a, b: (n, a-1, b-1), map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, inpt[1].splitlines()))))
 for n, a, b in moves:
   for _ in range(n):
@@ -13,4 +14,4 @@ for n, a, b in moves:
   crane2[a] = crane2[a][:-n]
 print("".join(map(itemgetter(-1), crane1)), "".join(map(itemgetter(-1), crane2)))
 
-print(""*(0 if (crane1 := list(map(list, stacks := list(filter(None, map(lambda s: list(filter(str.isalpha, s)), map(list, map(reversed, zip(*(inpt := Path("day5.txt").read_text().split("\n\n", maxsplit = 1))[0].splitlines()[:-1])))))))), crane2 := list(map(list, stacks)), list(starmap(lambda n, a, b: [crane1[b].append(crane1[a].pop()) for _ in range(n)], moves := list(starmap(lambda n, a, b: (n, a-1, b-1), map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, inpt[1].splitlines())))))), list(starmap(lambda n, a, b: (crane2[b].extend(crane2[a][-n:]), [crane2[a].pop() for _ in range(n)]), moves))) else 0) + "".join(map(itemgetter(-1), crane1)), "".join(map(itemgetter(-1), crane2)))
+print(""*(0 if (crane1 := list(map(list, crane2 := list(filter(None, map(lambda s: list(filter(str.isalpha, s)), map(list, map(reversed, zip(*(inpt := Path("day5.txt").read_text().split("\n\n", maxsplit = 1))[0].splitlines()[:-1])))))))), list(starmap(lambda n, a, b: [crane1[b].append(crane1[a].pop()) for _ in range(n)], moves := list(starmap(lambda n, a, b: (n, a-1, b-1), map(lambda m: list(map(int, filter(str.isnumeric, m))), map(str.split, inpt[1].splitlines())))))), list(starmap(lambda n, a, b: (crane2[b].extend(crane2[a][-n:]), [crane2[a].pop() for _ in range(n)]), moves))) else 0) + "".join(map(itemgetter(-1), crane1)), "".join(map(itemgetter(-1), crane2)))
