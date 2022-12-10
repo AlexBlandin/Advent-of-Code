2f143339990af26e05d4f9f4d80e00e41f0cc1a6
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-10 23:50:40 +0000

    Undo format because 20 lines is very nice

--- 2022/day8.py
+++ 2022/day8.py
@@ -13,15 +13,8 @@ def peaks(row: list[int]):
   return out
 
 trees = [list(map(int, line)) for line in Path("day8.txt").read_text().splitlines()]
-W, H = len(trees[0]), len(trees)
-transpose, backwards = lambda t: list(map(list, zip(*t))), lambda t: list(map(lambda line: list(reversed(line)), t))
-forest = [[peaks(row) for row in trees]
-          for trees in [trees, backwards(trees), transpose(trees),
-                        backwards(transpose(trees))]]
-sightlines = [
-  (vis, (W - x - 1, x, H - y - 1, y))
-  for y, rows in enumerate(zip(forest[0], backwards(forest[1]), transpose(forest[2]), transpose(backwards(forest[3]))))
-  for x, vis in enumerate(zip(*rows))
-]
+W, H, transpose, backwards = len(trees[0]), len(trees), lambda t: list(map(list, zip(*t))), lambda t: list(map(lambda line: list(reversed(line)), t))
+forest = [[peaks(row) for row in trees] for trees in [trees, backwards(trees), transpose(trees), backwards(transpose(trees))]]
+sightlines = [(vis, (W - x - 1, x, H - y - 1, y)) for y, rows in enumerate(zip(forest[0], backwards(forest[1]), transpose(forest[2]), transpose(backwards(forest[3])))) for x, vis in enumerate(zip(*rows))]
 
 print(sum(0 in vis for vis, _ in sightlines), max(prod(v if v else d for v, d in zip(vs, ds)) for vs, ds in sightlines))
