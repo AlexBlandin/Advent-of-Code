feec582bf79adecff367b58ab34b29d8c6ae0614
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-14 15:56:43 +0000

    Obvious cleanup

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
 ops = list(map(str.split, Path("day10.txt").read_text().splitlines()))
-x, c, s, screen = 1, 1, 0, [[False for _ in range(40)] for _ in range(6)]
+x, c, s, screen = 1, 0, 0, [[False for _ in range(40)] for _ in range(6)]
+
 def step(c, x):
-  j, i = divmod(c - 1, 40)
-  if i in (x - 1, x, x + 1): screen[j][i] = True
-  return c * x if (c - 20) % 40 == 0 else 0
+  j, i = divmod(c, 40)
+  if x - 1 <= i <= x + 1: screen[j][i] = True
+  return 0 if (c - 19) % 40 else (c + 1) * x
+
 for op in ops:
   s += step(c, x)
   c += 1
