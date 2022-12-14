7ca6bbd65020ad18f7f27bd26d9b21e138739e42
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-14 15:19:27 +0000

    Day 10

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,3 +1,17 @@
 from pathlib import Path
 
-lines = Path("day10.txt").read_text().splitlines()
+ops = list(map(str.split, Path("day10.txt").read_text().splitlines()))
+x, c, s, screen = 1, 1, 0, [[False for _ in range(40)] for _ in range(6)]
+def step(c, x):
+  j, i = divmod(c - 1, 40)
+  if i in (x - 1, x, x + 1): screen[j][i] = True
+  return c * x if (c - 20) % 40 == 0 else 0
+for op in ops:
+  s += step(c, x)
+  c += 1
+  if len(op) == 2:
+    s += step(c, x)
+    c += 1
+    x += int(op[1])
+
+print(s, "\n".join("".join("#" if c else "." for c in row) for row in screen), sep = "\n")
--- 2022/day9.py
+++ 2022/day9.py
@@ -1,3 +1,38 @@
 from pathlib import Path
 
-lines = Path("day9.txt").read_text().splitlines()
+D = dict(zip("UDLRN", zip(range(4), [(0, -1), (0, 1), (-1, 0), (1, 0), (0, 0)])))
+moves = [(D[d], int(n)) for d, n in map(str.split, Path("day9.txt").read_text().splitlines())]
+moves = [(D["R"], 4), (D["U"], 4), (D["L"], 3), (D["D"], 1), (D["R"], 4), (D["D"], 1), (D["L"], 5), (D["R"], 2)]
+
+rel_to_step: dict[tuple[int, int], int] = dict(
+  zip(((-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)), range(9))
+)
+# (-1,-1) ( 0,-1) ( 1,-1)
+# (-1, 0) ( 0, 0) ( 1, 0)
+# (-1, 1) ( 0, 1) ( 1, 1)
+
+# step[rel_to_step[T_rel]][v: UDLR.values] = T_rel_new # relative (x,y) space around H
+step: list[list[tuple[tuple[int, int], int]]] = [
+  [((-1, 0), -1), ((0, -1), 3), ((0, -1), 0), ((-1, 0), 1), ((-1, -1), -1)],
+  [((0, 0), -1), ((0, -1), 1), ((1, -1), -1), ((-1, -1), -1), ((0, -1), -1)],
+  [((1, 0), -1), ((0, -1), 2), ((1, 0), 1), ((0, -1), -1), ((1, -1), -1)],
+  [((-1, 1), -1), ((-1, -1), -1), ((0, 0), -1), ((-1, 0), 3), ((-1, 0), -1)],
+  [((0, 1), -1), ((0, -1), -1), ((1, 0), -1), ((-1, 0), -1), ((0, 0), -1)],
+  [((1, 1), -1), ((1, -1), -1), ((1, 0), 2), ((0, 0), -1), ((1, 0), -1)],
+  [((0, 1), 3), ((-1, 0), -1), ((0, 1), -1), ((-1, 0), 0), ((-1, 1), -1)],
+  [((0, 1), 0), ((0, 0), -1), ((1, 1), -1), ((-1, 1), -1), ((0, 1), -1)],
+  [((0, 1), 2), ((1, 0), -1), ((1, 0), 0), ((0, 1), -1), ((1, 1), -1)],
+] # TODO: augment with tail's "direction" so we can carry forward
+
+knots: list[tuple[int, int]] = [(0, 0) for i in range(10)]
+v1, v9 = {knots[1]}, {knots[9]}
+for (d, (x, y)), n in moves: # moves contains the initial d
+  for _ in range(n):
+    for i, ((hx, hy), (tx, ty)) in list(enumerate(zip(knots[:], knots[1:]), 1)): # copy so don't need to go backwards
+      knots[i], d = step[rel_to_step[hx - tx, hy - ty]][d] # the issue is everything moves in d, which is wrong
+      if i == 1: v1.add(knots[i])
+      if i == 9: v9.add(knots[i])
+    hx, hy = knots[0]
+    knots[0] = hx + x, hy + y
+
+print(len(v1), len(v9))
