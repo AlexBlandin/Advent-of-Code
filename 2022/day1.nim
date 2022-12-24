628f4f9e6615efc25fa2abf39fcd1c41fcd2f72b
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2022-12-24 13:20:58 +0000

    Looked into sugar and saw =>

--- 2022/day1.nim
+++ 2022/day1.nim
@@ -1,9 +1,7 @@
 import sugar, math, algorithm
 include prelude
 
-var elves = collect(newSeq):
-  for elf in "day1.txt".readFile.split("\n\n"):
-    sum elf.split.map parseInt
+var elves = "day1.txt".readFile.split("\n\n").map(elf => sum elf.split.map parseInt)
 elves.sort
 
 echo &"{elves[high(elves)]} {elves[high(elves)-2..high(elves)].sum}"
