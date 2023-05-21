023cc2ff14e663c9c71d017facf1a7926f6db8f3
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2023-05-21 19:13:08 +0100

    Day 11, belated completion

--- 2022/day11.py
+++ 2022/day11.py
@@ -11,14 +11,10 @@ LINES = Path("day11.txt").read_text().replace(",", "").replace("* old", "** 2").
 ITEMS = list(last_word(LINES[1::7], slice(2, None), lambda xs: list(map(int, xs))))
 OP = {"+": add, "*": mul, "**": pow}
 OPS = [partial(op, exp = v) if op is pow else partial(op, v) for op, v in zip(last_word(LINES[2::7], -2, post = OP.get), last_word(LINES[2::7]))]
-MODULOS = list(last_word(LINES[3::7]))
-TTHROW = last_word(LINES[4::7])
-FTHROW = last_word(LINES[5::7])
+MODULOS, TTHROW, FTHROW = list(last_word(LINES[3::7])), last_word(LINES[4::7]), last_word(LINES[5::7])
 TEST = map(lambda m, t, f: lambda x: f if x % m else t, MODULOS, TTHROW, FTHROW)
-PACKED = list(zip(OPS, TEST))
-LCM = lcm(*MODULOS)
 
-def play(n = 20, relief = False, unpack = PACKED, items = ITEMS):
+def play(n = 20, relief = False, items = ITEMS, unpack = list(zip(OPS, TEST)), lcm = lcm(*MODULOS)):
   seen = [0 for _ in items]
   item = [(monkey, worry) for monkey, holding in enumerate(items) for worry in holding]
   for t, v in item:
@@ -30,7 +26,7 @@ def play(n = 20, relief = False, unpack = PACKED, items = ITEMS):
         v = op(v)
         if relief:
           v //= 3
-        v %= LCM
+        v %= lcm
         t, g = g, th(v)
       t = g
   return prod(sorted(seen)[-2:])
