ee26d626215b8281a798c10815828e0e1058fd37
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2023-06-03 22:08:01 +0100

    Simplify

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,15 +1,13 @@
-from itertools import chain, count
+from itertools import repeat, count, chain
 from operator import itemgetter
 from pathlib import Path
 
-lines = Path("day14.txt").read_text().splitlines()
-collide: set[tuple[int, int]] = set()
-for frm, to in chain.from_iterable(map(lambda lst: list(zip(lst, lst[1:])), map(lambda s: s.split(" -> "), lines))):
-  (ax, ay), (bx, by) = map(int, frm.split(",", maxsplit = 1)), map(int, to.split(",", maxsplit = 1))
+lines, collide = Path("day14.txt").read_text().splitlines(), set()
+for a, b in chain.from_iterable(map(lambda lst: list(zip(lst, lst[1:])), map(lambda s: s.split(" -> "), lines))):
+  (ax, ay), (bx, by) = map(int, a.split(",", maxsplit = 1)), map(int, b.split(",", maxsplit = 1))
   collide |= {(ax, y) for y in range(min(ay, by), max(ay, by) + 1)} if ax == bx else {(x, ay) for x in range(min(ax, bx), max(ax, bx) + 1)}
 ORIGIN, BELOW = (500, 0), max(collide, key = itemgetter(1))[1]
-for i in count():
-  x, y = ORIGIN
+for i, (x, y) in zip(count(), repeat(ORIGIN)):
   while True:
     if y > BELOW:
       break
@@ -24,8 +22,7 @@ for i in count():
       break
   if y > BELOW:
     break
-for j in count(i + 1): # since the i'th fell into the void
-  x, y = ORIGIN
+for j, (x, y) in zip(count(i + 1), repeat(ORIGIN)): # since the i'th fell into the void
   while True:
     if (down := (x, y + 1)) not in collide and y < BELOW + 1:
       x, y = down
