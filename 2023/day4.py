ee0d4944dc6eb4f7fd9a0d2aa47ad1b44fe056e8
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-07 03:16:36 +0000

    Day 4

--- 2023/day4.py
+++ 2023/day4.py
@@ -1,3 +1,11 @@
 from pathlib import Path
 
-lines = Path("day4.txt").read_text().splitlines()
+wins_per = [len(set.intersection(*map(lambda s: set(s.split()), line.split(": ")[1].split(" | ")))) for line in Path("day4.txt").read_text().splitlines()]
+
+copies = [1] * len(wins_per)
+for i in range(len(wins_per)):
+  c = copies[i]
+  for j in range(i + 1, min(i + wins_per[i] + 1, len(wins_per))):
+    copies[j] += c
+
+print(sum(1 << (win - 1) if win else 0 for win in wins_per), sum(copies))
