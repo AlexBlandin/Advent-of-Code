39a1806b87c256512fbb10777b57e8656063305b
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-10 01:11:37 +0000

    Day 6

--- 2023/day6.py
+++ 2023/day6.py
@@ -1,3 +1,21 @@
+from itertools import repeat
+from math import prod
 from pathlib import Path
 
-lines = Path("day6.txt").read_text().splitlines()
+
+def will_travel(ms_held: int, race_duration: int, _):
+  return (race_duration - ms_held) * ms_held
+
+
+Time, Dist, *_ = Path("day6.txt").read_text().splitlines()
+
+print(
+  prod(
+    sum(1 for _ in filter(lambda x: will_travel(*x) > x[2], zip(range(t), repeat(t), repeat(d), strict=False)))
+    for t, d in zip(map(int, Time.split()[1:]), map(int, Dist.split()[1:]), strict=True)
+  ),
+  next(
+    sum(1 for _ in filter(lambda x: will_travel(*x) > x[2], zip(range(t), repeat(t), repeat(d), strict=False)))
+    for t, d in [(int(Time.split(maxsplit=1)[1].replace(" ", "")), int(Dist.split(maxsplit=1)[1].replace(" ", "")))]
+  ),
+)
