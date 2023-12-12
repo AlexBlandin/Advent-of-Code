dfd87db83954850d9713842d90064f62790b2d89
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-12 14:13:23 +0000

    Cleanup because yes that is always the case

--- 2023/day8.py
+++ 2023/day8.py
@@ -8,18 +8,13 @@ leftright = list(map(lambda line: 0 if line == "L" else 1, lines[0]))
 nodes = {k: (left, right) for k, left, right in map(str.split, lines[2:])}
 goal = {node for node in just_nodes if node.endswith("Z")}
 
-p1_node, p1_found_in = "AAA", None
-p2_nodes, p2_found_in, p2_found = [node for node in just_nodes if node.endswith("A")], [None for _ in goal], [False for _ in goal]
+path, found_in, found = [node for node in just_nodes if node.endswith("A")], [None for _ in goal], [False for _ in goal]
 for i, lr in enumerate(chain.from_iterable(repeat(leftright))):
-  if p1_node == "ZZZ" and p1_found_in is None:
-    p1_found_in = i
-  elif p1_found_in is None:
-    p1_node = nodes[p1_node][lr]
   for j in range(len(goal)):
-    if p2_nodes[j].endswith("Z") and not p2_found[j]:
-      p2_found_in[j] = i
-      p2_found[j] = True
-    p2_nodes[j] = nodes[p2_nodes[j]][lr]
-  if all(p2_found):
+    if path[j].endswith("Z") and not found[j]:
+      found_in[j] = i
+      found[j] = True
+    path[j] = nodes[path[j]][lr]
+  if all(found):
     break
-print(p1_found_in, math.lcm(*p2_found_in))
+print(found_in[0], math.lcm(*found_in))
