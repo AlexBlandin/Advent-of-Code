5dd876e1906b0d8d092ee86503c984906562e158
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2023-12-13 23:05:58 +0000

    Day 10 part 1

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,3 +1,91 @@
+from collections import Counter
+from enum import Enum
+from itertools import combinations
+from operator import itemgetter
 from pathlib import Path
 
-lines = Path("day10.txt").read_text().splitlines()
+grid = Path("day10.txt").read_text().splitlines()
+S = (0, 0)
+for y, row in enumerate(grid):
+  for x, c in enumerate(row):
+    if c == "S":
+      S = (x, y)
+      break
+  else:
+    continue
+  break
+pos = was = S
+
+
+class D(Enum):
+  NORTH = (0, -1)
+  SOUTH = (0, 1)
+  EAST = (1, 0)
+  WEST = (-1, 0)
+
+
+UNDER = ""
+for (x, y), (a, b) in combinations((D.NORTH.value, D.EAST.value, D.SOUTH.value, D.WEST.value), 2):
+  match [(x, y), grid[S[1] + y][S[0] + x], (a, b), grid[S[1] + b][S[0] + a]]:
+    case [D.NORTH.value, "|" | "7" | "F", D.SOUTH.value, "|" | "L" | "J"]:
+      UNDER = "|"
+      was = S[0] + a, S[1] + b
+      break
+    case [D.NORTH.value, "|" | "7" | "F", D.EAST.value, "-" | "J" | "7"]:
+      UNDER = "L"
+      was = S[0] + a, S[1] + b
+      break
+    case [D.NORTH.value, "|" | "7" | "F", D.WEST.value, "-" | "L" | "F"]:
+      UNDER = "J"
+      was = S[0] + a, S[1] + b
+      break
+    case [D.EAST.value, "-" | "J" | "7", D.SOUTH.value, "|" | "L" | "J"]:
+      UNDER = "F"
+      was = S[0] + a, S[1] + b
+      break
+    case [D.EAST.value, "-" | "J" | "7", D.WEST.value, "-" | "L" | "F"]:
+      UNDER = "-"
+      was = S[0] + a, S[1] + b
+      break
+    case [D.SOUTH.value, "|" | "L" | "J", D.WEST.value, "-" | "L" | "F"]:
+      UNDER = "7"
+      was = S[0] + a, S[1] + b
+      break
+grid[S[1]] = grid[S[1]].replace("S", UNDER)
+loop: list[tuple[int, int]] = []
+while True:
+  loop.append(pos)
+  match [grid[pos[1]][pos[0]], (was[0] - pos[0], was[1] - pos[1])]:
+    case ["|", D.NORTH.value]:
+      pos, was = (pos[0], pos[1] + 1), pos
+    case ["|", D.SOUTH.value]:
+      pos, was = (pos[0], pos[1] - 1), pos
+    case ["-", D.EAST.value]:
+      pos, was = (pos[0] - 1, pos[1]), pos
+    case ["-", D.WEST.value]:
+      pos, was = (pos[0] + 1, pos[1]), pos
+    case ["L", D.NORTH.value]:
+      pos, was = (pos[0] + 1, pos[1]), pos
+    case ["L", D.EAST.value]:
+      pos, was = (pos[0], pos[1] - 1), pos
+    case ["J", D.NORTH.value]:
+      pos, was = (pos[0] - 1, pos[1]), pos
+    case ["J", D.WEST.value]:
+      pos, was = (pos[0], pos[1] - 1), pos
+    case ["F", D.SOUTH.value]:
+      pos, was = (pos[0] + 1, pos[1]), pos
+    case ["F", D.EAST.value]:
+      pos, was = (pos[0], pos[1] + 1), pos
+    case ["7", D.SOUTH.value]:
+      pos, was = (pos[0] - 1, pos[1]), pos
+    case ["7", D.WEST.value]:
+      pos, was = (pos[0], pos[1] + 1), pos
+
+  if pos == S:
+    break
+
+xs, ys = Counter(map(itemgetter(0), loop)), Counter(map(itemgetter(1), loop))
+print(xs.most_common())
+print(ys.most_common())
+
+print(len(loop) // 2)
