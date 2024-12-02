35c24a6590a03e1d57346c23a54b9222844ece72
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2024-12-02 21:14:56 +0000

    Added the dreaded one-liner

--- python/advent2024/day1.py
+++ python/advent2024/day1.py
@@ -3,12 +3,6 @@ from operator import itemgetter, sub
 from pathlib import Path
 
 lines = Path("day1.txt").read_text().splitlines()
-# lines = """3   4
-# 4   3
-# 2   5
-# 1   3
-# 3   9
-# 3   3""".splitlines()
 
 pairs: list[tuple[int, int]] = [tuple(map(int, line.split())) for line in lines]
 left, right = sorted(map(int, map(itemgetter(0), pairs))), sorted(map(int, map(itemgetter(1), pairs)))
@@ -20,3 +14,8 @@ print(
   sum(diff),
   sum(similarity),
 )
+
+# ruff: noqa: B905 E501 E402 I001 F811 E401
+# fmt: off
+import collections, itertools, operator, pathlib
+print(*(sum(foo(bar(a)))for foo, bar, a in zip((lambda a: map(abs, map(operator.sub, next(a), next(a))), lambda a: (n * a[1][n] for n in a[0])), (lambda a: iter(a), lambda a: (next(a), collections.Counter(next(a)))), itertools.tee(list(map(sorted, zip(*(map(int, line.split()) for line in pathlib.Path("day1.txt").read_text().splitlines()))))))))
