090193f81b356c23d9104da492f997c2a07f588d
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2024-12-10 21:07:56 +0000

    Day 4 goof: thought X included rotation (+), oops!

--- python/advent2024/day4.py
+++ python/advent2024/day4.py
@@ -2,33 +2,16 @@ from collections import defaultdict
 from pathlib import Path
 
 lines = Path("day4.txt").read_text().splitlines()
-_lines = """MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX""".splitlines()
 trans = ["".join(s) for s in zip(*lines)]
-assert len(lines) == len(trans)  # squares are easier to reason about # noqa: S101
-diagonals: defaultdict[int, list[str]] = defaultdict(list)
-antidiags: defaultdict[int, list[str]] = defaultdict(list)
+diagonals, antidiags = defaultdict(list), defaultdict(list)
 for i, row in enumerate(lines):
   for j, a in enumerate(row):
     diagonals[i + j].append(a)
     antidiags[i - j].append(a)
-diags = ["".join(v) for v in diagonals.values()]
-adias = ["".join(v) for v in antidiags.values()]
+diags, adias = ["".join(v) for v in diagonals.values()], ["".join(v) for v in antidiags.values()]
 xmas = {
   tuple(g.translate({ord("1"): letter1, ord("2"): letter2, ord("3"): letter3, ord("4"): letter4}) for g in group)
   for group in (
-    ("1M2", "MAS", "3S4"),
-    ("1S2", "MAS", "3M4"),
-    ("1S2", "SAM", "3M4"),
-    ("1M2", "SAM", "3S4"),
     ("M1S", "2A3", "M4S"),
     ("S1M", "2A3", "S4M"),
     ("M1M", "2A3", "S4S"),
@@ -39,40 +22,16 @@ xmas = {
   for letter3 in "XMAS"
   for letter4 in "XMAS"
 }
-xxmas = {
-  tuple(g.translate({ord("1"): letter1, ord("2"): letter2, ord("3"): letter3, ord("4"): letter4}) for g in group)
-  for group in (
-    ("1M2", "MAS", "3S4"),
-    ("1S2", "MAS", "3M4"),
-    ("1S2", "SAM", "3M4"),
-    ("1M2", "SAM", "3S4"),
-    ("M1S", "2A3", "M4S"),
-    ("S1M", "2A3", "S4M"),
-    ("M1M", "2A3", "S4S"),
-    ("S1S", "2A3", "M4M"),
-  )
-  for letter1 in "MS"
-  for letter2 in "MS"
-  for letter3 in "MS"
-  for letter4 in "MS"
-  if letter1 != letter4 and letter2 != letter3
-}
-mx = len(lines) - 1
-
-
-def nearby(i: int, j: int, lines=lines, mx=mx) -> tuple[str, str, str]:
-  return (
-    (lines[i - 1][j - 1 : j + 2], lines[i][j - 1 : j + 2], lines[i + 1][j - 1 : j + 2])
-    if 1 <= i < mx and 1 <= j < mx
-    else ("", "", "")
-  )
-
 
 print(
   sum(s.count("XMAS") + s.count("SAMX") for s in lines)
   + sum(s.count("XMAS") + s.count("SAMX") for s in trans)
   + sum(s.count("XMAS") + s.count("SAMX") for s in diags)
   + sum(s.count("XMAS") + s.count("SAMX") for s in adias),
-  sum((nearby(i, j) in xxmas) + (nearby(i, j) in xmas) for i in range(len(lines)) for j in range(len(trans))),
+  sum(
+    ((lines[i - 1][j - 1 : j + 2], lines[i][j - 1 : j + 2], lines[i + 1][j - 1 : j + 2]) in xmas)
+    for i in range(len(lines))
+    for j in range(len(trans))
+    if 1 <= i < len(lines) - 1 and 1 <= j < len(lines) - 1
+  ),
 )
-# 1933 is too high
