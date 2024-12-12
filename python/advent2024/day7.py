621b9d0a3d005d35f264dc37dbb1893099a07eb4
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2024-12-12 23:06:49 +0000

    7 is nice and quick to solve

--- python/advent2024/day7.py
+++ python/advent2024/day7.py
@@ -1,8 +1,36 @@
+import operator
+from functools import reduce
+from itertools import product
 from pathlib import Path
 
 lines = Path("day7.txt").read_text().splitlines()
 
+
+def chain_call(funcs: list[callable]):
+  _funcs = iter(funcs)
+
+  def _chain_call(x, y):
+    return next(_funcs)(x, y)
+
+  return _chain_call
+
+
+equations = [(int(test), list(map(int, values.split()))) for test, values in (line.split(": ") for line in lines)]
+
 print(
-  ...,
-  ...,
+  sum(
+    test
+    for test, values in equations
+    if any(
+      test == reduce(chain_call(ops), values) for ops in product((operator.add, operator.mul), repeat=len(values) - 1)
+    )
+  ),
+  sum(
+    test
+    for test, values in equations
+    if any(
+      test == reduce(chain_call(ops), values)
+      for ops in product((operator.add, operator.mul, lambda x, y: int(f"{x}{y}")), repeat=len(values) - 1)
+    )
+  ),
 )
